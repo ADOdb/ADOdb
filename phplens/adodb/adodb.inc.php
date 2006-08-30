8f37f9194adc981e0a935993f7b71a0f70f9a7ac
John Lim <jlim@juristech.net>
2006-08-30 03:41:36 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/adodb/code@95 d66a17b1-268b-459a-bce8-d49450bab0c9

--- phplens/adodb/adodb.inc.php
+++ phplens/adodb/adodb.inc.php
@@ -14,7 +14,7 @@
 /**
 	\mainpage 	
 	
-	 @version V4.92 29 Aug 2006  (c) 2000-2006 John Lim (jlim#natsoft.com.my). All rights reserved.
+	 @version V4.92a 29 Aug 2006  (c) 2000-2006 John Lim (jlim#natsoft.com.my). All rights reserved.
 
 	Released under both BSD license and Lesser GPL library license. You can choose which license
 	you prefer.
