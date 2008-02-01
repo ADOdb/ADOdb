425e058325b918ed8739f2c697011b149d5c0fab
John Lim <jlim@juristech.net>
2008-02-01 04:14:16 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/adodb/code@205 d66a17b1-268b-459a-bce8-d49450bab0c9

--- phplens/adodb5/adodb-time.inc.php
+++ phplens/adodb5/adodb-time.inc.php
@@ -242,6 +242,9 @@ b. Implement daylight savings, which looks awfully complicated, see
 
 CHANGELOG
 
+- 1 Feb 2008 0.32
+* Now adodb_mktime(0,0,0,12+$m,20,2040) works properly. 
+
 - 10 Jan 2008 0.31
 * Now adodb_mktime(0,0,0,24,1,2037) works correctly.
 
@@ -380,7 +383,7 @@ First implementation.
 /*
 	Version Number
 */
-define('ADODB_DATE_VERSION',0.31);
+define('ADODB_DATE_VERSION',0.32);
 
 $ADODB_DATETIME_CLASS = (PHP_VERSION >= 5.2);
 
@@ -426,9 +429,12 @@ function adodb_date_test_strftime($fmt)
 
 /**
 	 Test Suite
-*/
+*/	
 function adodb_date_test()
 {
+	
+	for ($m=-24; $m<=24; $m++)
+		echo "$m :",adodb_date('d-m-Y',adodb_mktime(0,0,0,1+$m,20,2040)),"<br>";
 	
 	error_reporting(E_ALL);
 	print "<h4>Testing adodb_date and adodb_mktime. version=".ADODB_DATE_VERSION.' PHP='.PHP_VERSION."</h4>";
@@ -1237,7 +1243,7 @@ function adodb_mktime($hr,$min,$sec,$mon=false,$day=false,$year=false,$is_dst=fa
 	$year = adodb_year_digit_check($year);
 
 	if ($mon > 12) {
-		$y = floor($mon / 12);
+		$y = floor(($mon-1)/ 12);
 		$year += $y;
 		$mon -= $y*12;
 	} else if ($mon < 1) {
