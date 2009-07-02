5cf1ee4c3ba19cb06cbd42d359ea8232e03cce90
John Lim <jlim@juristech.net>
2009-07-02 13:26:46 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/adodb/code@375 d66a17b1-268b-459a-bce8-d49450bab0c9

--- phplens/adodb5/pear/Auth/Container/ADOdb.php
+++ phplens/adodb5/pear/Auth/Container/ADOdb.php
@@ -1,24 +1,16 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// |                                                                      |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Martin Jansen <mj@php.net>
-// |	Richard Tango-Lowy <richtl@arscognita.com>                                  |
-// +----------------------------------------------------------------------+
-//
-// $Id: ADOdb.php,v 1.3 2005/05/18 06:58:47 jlim Exp $
-//
+/* 
+V5.09 25 June 2009   (c) 2000-2009 John Lim (jlim#natsoft.com). All rights reserved.
+  Released under both BSD license and Lesser GPL library license. 
+  Whenever there is any discrepancy between the two licenses, 
+  the BSD license will take precedence. See License.txt. 
+  Set tabs to 4 for best viewing.
+  
+  Latest version is available at http://adodb.sourceforge.net
+  
+	Original Authors: Martin Jansen <mj#php.net>
+	Richard Tango-Lowy <richtl#arscognita.com>                          
+*/
 
 require_once 'Auth/Container.php';
 require_once 'adodb.inc.php';
