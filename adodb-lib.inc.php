d77e5c59936e0deaf1abd88baada674710111797
Damien Regad <dregad@mantisbt.org>
2014-02-24 14:46:43 +0100

    adodb-lib: Fix columns quoting bug
    See https://sourceforge.net/p/adodb/bugs/127/

--- adodb-lib.inc.php
+++ adodb-lib.inc.php
@@ -645,7 +645,7 @@ function _adodb_getupdatesql(&$zthis,&$rs, $arrFields,$forceUpdate=false,$magicq
 					}
 
 					if ((strpos($upperfname,' ') !== false) || ($ADODB_QUOTE_FIELDNAMES)) {
-						switch (ADODB_QUOTE_FIELDNAMES) {
+						switch ($ADODB_QUOTE_FIELDNAMES) {
 						case 'LOWER':
 							$fnameq = $zthis->nameQuote.strtolower($field->name).$zthis->nameQuote;break;
 						case 'NATIVE':
@@ -817,7 +817,7 @@ static $cacheCols;
 		if (adodb_key_exists($upperfname,$arrFields,$force)) {
 			$bad = false;
 			if ((strpos($upperfname,' ') !== false) || ($ADODB_QUOTE_FIELDNAMES)) {
-				switch (ADODB_QUOTE_FIELDNAMES) {
+				switch ($ADODB_QUOTE_FIELDNAMES) {
 				case 'LOWER':
 					$fnameq = $zthis->nameQuote.strtolower($field->name).$zthis->nameQuote;break;
 				case 'NATIVE':
