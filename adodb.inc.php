ADOdb/ADOdb

commit 30c21280a48680c079f825af6583793928d65cf5

Fix version match regex to include suffix
The old regex in ADOConnection::Version only included the numeric part
of the ADOdb version number. Now the suffix (dev|[1-z]) is also
returned.

The regex in updateversion.py has been updated to match.

adodb.inc.php
@@ -471,7 +471,7 @@
 	{
 	global $ADODB_vers;
 
-		$ok = preg_match( '/^[Vv]([0-9\.]+)/', $ADODB_vers, $matches );
+		$ok = preg_match( '/^[Vv]?([0-9]\.[0-9]+(dev|[a-z]))?/', $ADODB_vers, $matches );
 		if (!$ok) return (float) substr($ADODB_vers,1);
 		else return $matches[1];
 	}

scripts/updateversion.py
@@ -17,7 +17,7 @@ import sys
 
 # ADOdb version validation regex
 _version_dev = "dev"
-_version_regex = "[Vv]?[0-9]\.[0-9]+([a-z]|%s)?" % _version_dev
+_version_regex = "[Vv]?[0-9]\.[0-9]+(%s|[a-z])?" % _version_dev
 _release_date_regex = "[0-9?]+.*[0-9]+"
 
 _tag_prefix = "v"

commit 701798f02ad2f06f7bba9e3922a2f6a9d8f05918

Fix version match regex
There was a small mistake in the version matching regex introduced in
commit 30c21280a48680c079f825af6583793928d65cf5, causing the function to
trigger a system notice and only return 'V' instead of the actual
version number.

Fixes #16

adodb.inc.php
@@ -461,7 +461,7 @@ if (!defined('_ADODB_LAYER')) {
 	{
 	global $ADODB_vers;
 
-		$ok = preg_match( '/^[Vv]?([0-9]\.[0-9]+(dev|[a-z]))?/', $ADODB_vers, $matches );
+		$ok = preg_match( '/^[Vv]?([0-9]\.[0-9]+(dev|[a-z])?)/', $ADODB_vers, $matches );
 		if (!$ok) return (float) substr($ADODB_vers,1);
 		else return $matches[1];
 	}

docs/docs-adodb.htm
@@ -6172,6 +6172,7 @@ PHP</a>. </p>
 
 <p><a name=5.20><b>5.20 - ??-???-2014</b>
 <p>adodb: AutoExecute correctly handles empty result set in case of updates. See Github #13.
+<p>adodb: Fix regex in Version(). See Github #16
 <p>memcache: use include_once() to avoid issues with PHPUnit. See http://phplens.com/lens/lensforum/msgs.php?id=19489
 <p>mssqlnative: Use ADOConnection::outp instead of error_log. See Github #12
 