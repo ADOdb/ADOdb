ADOdb/ADOdb

commit 52a1d8f473d90ec7f46d27a9b0439478e74659a9

GetAssoc issue when 2nd column is null
If the second column of the first row happens to be null, ADOdb returned
a one-based array rather than the expected zero-based one.

Resolved by using array_key_exists() instead of isset().

Fixes sourceforge bug #130

adodb.inc.php
@@ -3168,7 +3168,7 @@ http://www.stanford.edu/dept/itss/docs/oracle/10g/server.101/b10759/statements_1
 			$false = false;
 			return $false;
 		}
-		$numIndex = isset($this->fields[0]) && isset($this->fields[1]);
+		$numIndex = is_array($this->fields) && array_key_exists(0, $this->fields);
 		$results = array();
 
 		if (!$first2cols && ($cols > 2 || $force_array)) {

commit 32a479a182c86f4b15c337ca6b1d3f4b967e37a8

Fix incorrect behavior for GetAssoc()
Commit 52a1d8f473d90ec7f46d27a9b0439478e74659a9 introduced a regression.

The fix did not properly consider that the purpose of the isset tests
was to determine whether the recordset's fields was a numerically
0-based indexed array.

As a consequence, when calling GetAssoc() in ADODB_FETCH_ASSOC mode with
data like array('adodb', '0'), $numIndex was incorrectly set to True,
leading to an 'Undefined offset' PHP notice and wrong return value
array(0=>null) instead of array('adodb'=>'0').

Fixes #101

adodb.inc.php
@@ -3387,7 +3387,10 @@ http://www.stanford.edu/dept/itss/docs/oracle/10g/server.101/b10759/statements_1
 		if ($cols < 2) {
 			return false;
 		}
-		$numIndex = is_array($this->fields) && array_key_exists(0, $this->fields);
+
+		// Determine whether the array is associative or 0-based numeric
+		$numIndex = array_keys($this->fields) == range(0, count($this->fields) - 1);
+
 		$results = array();
 
 		if (!$first2cols && ($cols > 2 || $force_array)) {