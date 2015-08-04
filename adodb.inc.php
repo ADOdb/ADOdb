ADOdb/ADOdb

commit 41839f5c18abe262ee088f1d4bd92453616d2a4a

Add support for pagination with complex queries
There are cases where ADODB is not be able to figure out the proper
count(*) queries for PageExecute() on its own.

To work around this problem, ADOdb now handles a new keyword
"_ADODB_COUNT" that can be wrapped around the parts of the SQL query
that should be replaced with "COUNT(*)"; the code will remove everything
between and including the _ADODB_COUNT keywords and replace it with
COUNT(*). The _ADODB_COUNT tags are of course removed when COUNT(*) is
not needed.

Example usage:
    SELECT
        _ADODB_COUNT
        col1,
        col2,
        col3,
        ( select col4 from table2 where col1=col2 )
        col4,
        _ADODB_COUNT
    FROM table1
    LEFT JOIN ( select col5 from table3 where col3=col4 ) AS tmp;

Original 2012 bug report: http://phplens.com/lens/lensforum/msgs.php?id=19058
The fix has been in use since that time in hundreds of thousands of
production installations.

Fixes #88

Signed-off-by: Damien Regad <dregad@mantisbt.org>

adodb.inc.php
@@ -1122,8 +1122,13 @@ if (!defined('_ADODB_LAYER')) {
 		return $ret;
 	}
 
+	//Strips keyword used to help generate SELECT COUNT(*) queries from SQL if it exists.
+	function adodb_strip_count_keyword( $sql ) {
+		return ADODB_str_replace( '_ADODB_COUNT', '', $sql );
+	}
 
 	function _Execute($sql,$inputarr=false) {
+		$sql = $this->adodb_strip_count_keyword( $sql );
 		if ($this->debug) {
 			global $ADODB_INCLUDED_LIB;
 			if (empty($ADODB_INCLUDED_LIB)) {

adodb-lib.inc.php
@@ -421,8 +421,11 @@ function _adodb_getcount(&$zthis, $sql,$inputarr=false,$secs2cache=0)
 		}
 	} else {
 		// now replace SELECT ... FROM with SELECT COUNT(*) FROM
-		$rewritesql = preg_replace(
-					'/^\s*SELECT\s.*\s+FROM\s/Uis','SELECT COUNT(*) FROM ',$sql);
+		if ( strpos($sql, '_ADODB_COUNT') !== FALSE ) {
+			$rewritesql = preg_replace('/^\s*?SELECT\s+_ADODB_COUNT(.*)_ADODB_COUNT\s/is','SELECT COUNT(*) ',$sql);
+		} else {
+			$rewritesql = preg_replace('/^\s*?SELECT\s.*?\s+(.*?)\s+FROM\s/is','SELECT COUNT(*) FROM ',$sql);
+		}
 		// fix by alexander zhukov, alex#unipack.ru, because count(*) and 'order by' fails
 		// with mssql, access and postgresql. Also a good speedup optimization - skips sorting!
 		// also see http://phplens.com/lens/lensforum/msgs.php?id=12752

docs/docs-adodb.htm
@@ -6180,6 +6180,7 @@ PHP</a>. </p>
 <p>adodb: fix ADODB_Session::open() failing after successful ADONewConnection() call, thanks to Sjan Evardsson. See Github #44
 <p>adodb: Only include memcache library once for PHPUnit 4.x, thanks to Alan Farquharson. See Github #74
 <p>adodb: Move() returns false when given row is &lt; 0, thanks to Mike Benoit.
+<p>adodb: Add support for pagination with complex queries, thanks to Mike Benoit. See Github #88
 <p>datadict: oci8 strict-standards compliance. See Github #18
 <p>memcache: use include_once() to avoid issues with PHPUnit. See http://phplens.com/lens/lensforum/msgs.php?id=19489
 <p>mssqlnative: Use ADOConnection::outp instead of error_log. See Github #12

commit 52d1725e95ee30f28d4c6a9b3ec72c8e4d4e025f

Fix #105: error in _Execute() function
Commit 41839f5c18abe262ee088f1d4bd92453616d2a4a (fix for #88) introduced
a regression, causing error in some cases, for example "oci_execute()
expects parameter 1 to be resource, string given".

This resolves the problem by only attempting str_replace() on strings
rather than arrays.

Signed-off-by: Damien Regad <dregad@mantisbt.org>

Original patch was modified to wrap long comments and improve commit
message.

adodb.inc.php
@@ -1153,13 +1153,15 @@ if (!defined('_ADODB_LAYER')) {
 		return $ret;
 	}
 
-	//Strips keyword used to help generate SELECT COUNT(*) queries from SQL if it exists.
-	function adodb_strip_count_keyword( $sql ) {
-		return ADODB_str_replace( '_ADODB_COUNT', '', $sql );
-	}
-
 	function _Execute($sql,$inputarr=false) {
-		$sql = $this->adodb_strip_count_keyword( $sql );
+		// ExecuteCursor() may send non-string queries (such as arrays),
+		// so we need to ignore those.
+		if( is_string($sql) ) {
+			// Strips keyword used to help generate SELECT COUNT(*) queries
+			// from SQL if it exists.
+			$sql = ADODB_str_replace( '_ADODB_COUNT', '', $sql );
+		}
+
 		if ($this->debug) {
 			global $ADODB_INCLUDED_LIB;
 			if (empty($ADODB_INCLUDED_LIB)) {