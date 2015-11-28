10e108df500f7bf0bee5cea0db8c2a3714535e0c
Damien Regad <dregad@mantisbt.org>
2015-11-28 03:47:09 +0100

    Upload script: fix bug with -d / -f options
    The behavior was reverted, i.e. -d would disable documentation upload
    (instead of files), and vice-versa.
    
    Move check for version number check for files updload within the if
    block to avoid an error when uploading the documentation when the
    release tarballs are not present (e.g. from the repository's root).

--- scripts/uploadrelease.py
+++ scripts/uploadrelease.py
@@ -66,10 +66,10 @@ def main():
             sys.exit(0)
 
         elif opt in ("-f", "--files"):
-            upload_files = False
+            upload_doc = False
 
         elif opt in ("-d", "--doc"):
-            upload_doc = False
+            upload_files = False
 
     # Mandatory parameters
     username = args[0]
@@ -80,19 +80,19 @@ def main():
     except IndexError:
         release_path = os.getcwd()
 
-    # Get the version number from the zip file to upload
-    try:
-        zipfile = glob.glob('*.zip')[0]
-    except IndexError:
-        print "ERROR: release zip file not found in '%s'    " % release_path
-        sys.exit(1)
-    version = zipfile[5:8]
-
-    # Start upload process
-    print "ADOdb release upload script"
-
     # Upload release files
     if upload_files:
+        # Get the version number from the zip file to upload
+        try:
+            zipfile = glob.glob('*.zip')[0]
+        except IndexError:
+            print "ERROR: release zip file not found in '%s'    " % release_path
+            sys.exit(1)
+        version = zipfile[5:8]
+
+        # Start upload process
+        print "ADOdb release upload script"
+
         target = sf_files.format(ver=version)
         print
         print "Uploading release files..."
