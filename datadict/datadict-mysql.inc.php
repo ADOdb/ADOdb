afc8c7694cdc5328d6eccfa5fdde98343e277f7e
Ralf Becker <RalfBecker@outdoor-training.de>
2016-02-20 14:09:31 +0100

    Allow to specify a column specific character set eg. "CHARACTER SET ascii" via constrain parameter, required to be first part of suffix

--- datadict/datadict-mysql.inc.php
+++ datadict/datadict-mysql.inc.php
@@ -26,7 +26,7 @@ class ADODB2_mysql extends ADODB_DataDict {
 
 	function MetaType($t,$len=-1,$fieldobj=false)
 	{
-		
+
 		if (is_object($t)) {
 			$fieldobj = $t;
 			$t = $fieldobj->type;
@@ -114,6 +114,10 @@ class ADODB2_mysql extends ADODB_DataDict {
 	function _CreateSuffix($fname,&$ftype,$fnotnull,$fdefault,$fautoinc,$fconstraint,$funsigned)
 	{
 		$suffix = '';
+		if (stripos($fconstraint, 'CHARACTER SET') === 0) {
+			$suffix .= ' '.$fconstraint;
+			unset($fconstraint);
+		}
 		if ($funsigned) $suffix .= ' UNSIGNED';
 		if ($fnotnull) $suffix .= ' NOT NULL';
 		if (strlen($fdefault)) $suffix .= " DEFAULT $fdefault";
