1dcb11ca341d41e956688349dd479e1fdb5207c0
Ralf Becker <RalfBecker@outdoor-training.de>
2016-02-24 15:18:12 +0100

    fix pull request as discussed: remove unused assignment, square brackets around char class and assignment in if

--- datadict/datadict-postgres.inc.php
+++ datadict/datadict-postgres.inc.php
@@ -311,20 +311,22 @@ class ADODB2_postgres extends ADODB_DataDict {
 	{
 		if ($dropflds && !is_array($dropflds)) $dropflds = explode(',',$dropflds);
 		$copyflds = array();
-		foreach(($meta=$this->MetaColumns($tabname)) as $fld) {
+		foreach($this->MetaColumns($tabname) as $fld) {
 			if (!$dropflds || !in_array($fld->name,$dropflds)) {
 				// we need to explicit convert varchar to a number to be able to do an AlterColumn of a char column to a nummeric one
 				if (preg_match('/'.$fld->name.' (I|I2|I4|I8|N|F)/i',$tableflds,$matches) &&
 					in_array($fld->type,array('varchar','char','text','bytea'))) {
 					$copyflds[] = "to_number($fld->name,'S9999999999999D99')";
-				} elseif (preg_match('/'.$fld->name.' ([\w]+)/i',$tableflds,$matches) &&
-					strtoupper($fld->type) != ($type = $this->ActualType($matches[1]))) {
-					if ($type == 'BYTEA' && $fld->type == 'text') {
-						$copyflds[] = "DECODE($fld->name, 'escape')";
-					} elseif ($fld->type == 'bytea' && $type == 'TEXT') {
-						$copyflds[] = "ENCODE($fld->name, 'escape')";
-					} else {
-						$copyflds[] = "CAST($fld->name AS $type)";
+				} elseif (preg_match('/'.$fld->name.' (\w+)/i',$tableflds,$matches)) {
+					$type = $this->ActualType($matches[1]);
+					if (strtoupper($fld->type) != $type) {
+						if ($type == 'BYTEA' && $fld->type == 'text') {
+							$copyflds[] = "DECODE($fld->name, 'escape')";
+						} elseif ($fld->type == 'bytea' && $type == 'TEXT') {
+							$copyflds[] = "ENCODE($fld->name, 'escape')";
+						} else {
+							$copyflds[] = "CAST($fld->name AS $type)";
+						}
 					}
 				} else {
 					$copyflds[] = $fld->name;
