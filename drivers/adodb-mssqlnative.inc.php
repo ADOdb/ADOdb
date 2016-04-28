179b02b68593fd69b57e08125547bd3283f36dfb
Mark Newnham <mark@newnhams.com>
2016-04-28 17:26:58 -0600

    Driver does not support 'l' format in sqlDate , see #232
    The code for the 'l' format (day of week) is missing from the sqlDate function

--- drivers/adodb-mssqlnative.inc.php
+++ drivers/adodb-mssqlnative.inc.php
@@ -367,7 +367,9 @@ class ADODB_mssqlnative extends ADOConnection {
 			case 'A':
 				$s .= "substring(convert(char(19),$col,0),18,2)";
 				break;
-
+			case 'l':
+				$s.= "datename(dw,$col)";
+				break;
 			default:
 				if ($ch == '\\') {
 					$i++;
