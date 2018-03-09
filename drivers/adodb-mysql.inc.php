87be68cb6da0d7a7eb9e4729162fca4063549cef
Damien Regad <dregad@mantisbt.org>
2018-03-09 01:17:20 +0100

    mysql: die if extension is not available

--- drivers/adodb-mysql.inc.php
+++ drivers/adodb-mysql.inc.php
@@ -62,8 +62,17 @@ class ADODB_mysql extends ADOConnection {
 			);
 			die(1); // Stop execution even if not using Exceptions
 		} elseif(version_compare(PHP_VERSION, '5.5.0', '>=')) {
-			// Just print a warning
-			$this->outp('mysql extension is deprecated since PHP 5.5.0, consider using mysqli');
+			// If mysql extension is available just print a warning,
+			// otherwise die with an error message
+			if(function_exists('mysql_connect')) {
+				$this->outp('mysql extension is deprecated since PHP 5.5.0, consider using mysqli');
+			} else {
+				$this->outp_throw(
+					'mysql extension is not available, use mysqli instead',
+					__METHOD__
+				);
+				die(1); // Stop execution even if not using Exceptions
+			}
 		}
 	}
 
