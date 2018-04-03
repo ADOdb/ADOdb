ADOdb/ADOdb

commit 53d13c0f64ad97ff87fa3b74fa4a4e896e691c9b

Parse port out of hostname if specified when connecting
- ADOConnection::Connect
- ADOConnection::PConnect

Fixes #63

Signed-off-by: Damien Regad <dregad@mantisbt.org>

adodb.inc.php
@@ -571,6 +571,9 @@ if (!defined('_ADODB_LAYER')) {
 		if ($argHostname != "") {
 			$this->host = $argHostname;
 		}
+		if ( strpos($this->host, ':') > 0 && isset($this->port) ) {
+			list($this->host, $this->port) = explode(":", $this->host, 2);
+        	}
 		if ($argUsername != "") {
 			$this->user = $argUsername;
 		}
@@ -651,6 +654,9 @@ if (!defined('_ADODB_LAYER')) {
 		if ($argHostname != "") {
 			$this->host = $argHostname;
 		}
+		if ( strpos($this->host, ':') > 0 && isset($this->port) ) {
+			list($this->host, $this->port) = explode(":", $this->host, 2);
+	        }
 		if ($argUsername != "") {
 			$this->user = $argUsername;
 		}

docs/docs-adodb.htm
@@ -6181,6 +6181,7 @@ PHP</a>. </p>
 <p>adodb: Only include memcache library once for PHPUnit 4.x, thanks to Alan Farquharson. See Github #74
 <p>adodb: Move() returns false when given row is &lt; 0, thanks to Mike Benoit.
 <p>adodb: Add support for pagination with complex queries, thanks to Mike Benoit. See Github #88
+<p>adodb: Parse port out of hostname if specified in connection parameters, thanks to Andy Theuninck. See Github #63
 <p>datadict: oci8 strict-standards compliance. See Github #18
 <p>memcache: use include_once() to avoid issues with PHPUnit. See http://phplens.com/lens/lensforum/msgs.php?id=19489
 <p>mssqlnative: Use ADOConnection::outp instead of error_log. See Github #12

commit 3659bb49e0e416d375e6c56a9563f6add1d80b93

Fix LDAP connections using URIs
Commit 53d13c0f64ad97ff87fa3b74fa4a4e896e691c9b (fixing #58 and #63)
introduced a regression in 5.20.0 that broke connections LDAP when using
URIs like ldap:// or ldaps://.

Create a new protected method ADOConnection::parseHostNameAndPort() to
help parse off the port from a hostname while retaining the ldap:// or
ldaps:// scheme, and use it to refactor Connect() and PConnect().

Fixes #340

Signed-off-by: Damien Regad <dregad@mantisbt.org>

The PR's original commits were squashed and the message reworded. The
fix (initially targeted at master) was applied to hotfix/5.20 branch.

adodb.inc.php
@@ -431,6 +431,7 @@ if (!defined('_ADODB_LAYER')) {
 	var $databaseType = '';		/// RDBMS currently in use, eg. odbc, mysql, mssql
 	var $database = '';			/// Name of database to be used.
 	var $host = '';				/// The hostname of the database server
+	var $port = '';				/// The port of the database server
 	var $user = '';				/// The username which is used to connect to the database server.
 	var $password = '';			/// Password for the username. For security, we no longer store it.
 	var $debug = false;			/// if set to true will output sql statements
@@ -633,6 +634,26 @@ if (!defined('_ADODB_LAYER')) {
 	}
 
 	/**
+	 * Parses the hostname to extract the port.
+	 * Overwrites $this->host and $this->port, only if a port is specified.
+	 * The Hostname can be fully or partially qualified,
+	 * ie: "db.mydomain.com:5432" or "ldaps://ldap.mydomain.com:636"
+	 * Any specified scheme such as ldap:// or ldaps:// is maintained.
+	 */
+	protected function parseHostNameAndPort() {
+		$parsed_url = parse_url($this->host);
+		if (is_array($parsed_url) && isset($parsed_url['host']) && isset($parsed_url['port'])) {
+			if ( isset($parsed_url['scheme']) ) {
+				// If scheme is specified (ie: ldap:// or ldaps://, make sure we retain that.
+				$this->host = $parsed_url['scheme'] . "://" . $parsed_url['host'];
+			} else {
+				$this->host = $parsed_url['host'];
+			}
+			$this->port = $parsed_url['port'];
+		}
+	}
+
+	/**
 	 * Connect to database
 	 *
 	 * @param [argHostname]		Host to connect to
@@ -647,9 +668,9 @@ if (!defined('_ADODB_LAYER')) {
 		if ($argHostname != "") {
 			$this->host = $argHostname;
 		}
-		if ( strpos($this->host, ':') > 0 && isset($this->port) ) {
-			list($this->host, $this->port) = explode(":", $this->host, 2);
-        	}
+		// Overwrites $this->host and $this->port if a port is specified.
+		$this->parseHostNameAndPort();
+
 		if ($argUsername != "") {
 			$this->user = $argUsername;
 		}
@@ -730,9 +751,9 @@ if (!defined('_ADODB_LAYER')) {
 		if ($argHostname != "") {
 			$this->host = $argHostname;
 		}
-		if ( strpos($this->host, ':') > 0 && isset($this->port) ) {
-			list($this->host, $this->port) = explode(":", $this->host, 2);
-	        }
+		// Overwrites $this->host and $this->port if a port is specified.
+		$this->parseHostNameAndPort();
+
 		if ($argUsername != "") {
 			$this->user = $argUsername;
 		}