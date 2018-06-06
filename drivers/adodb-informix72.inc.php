19f92e2d1318c48b16ee950e7064cd4497e9a3ef
WiXSL <WiXSL@users.noreply.github.com>
2018-06-06 16:15:31 -0300

    Constructors shouldn't return a value

--- drivers/adodb-informix72.inc.php
+++ drivers/adodb-informix72.inc.php
@@ -403,7 +403,7 @@ class ADORecordset_informix72 extends ADORecordSet {
 			$mode = $ADODB_FETCH_MODE;
 		}
 		$this->fetchMode = $mode;
-		return parent::__construct($id);
+		parent::__construct($id);
 	}
 
 
