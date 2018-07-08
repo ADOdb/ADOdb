f6eb8e277e638054354e35f0b0094e005d3c3b3f
Mark Newnham <mark@newnhams.com>
2018-07-08 16:20:47 -0600

    Complex $sql confuses bind marker replacement see #370
    A non-regexp based function has been introduced to replace a regexp based section of code that has been subject  to numerous tweaks as more extreme test cases have appeared. This  is now done this like this to help maintainability and avoid the need to rely on regexp experienced maintainers.

--- drivers/adodb-oci8po.inc.php
+++ drivers/adodb-oci8po.inc.php
@@ -103,6 +103,84 @@ class ADODB_oci8po extends ADODB_oci8 {
 		}
 		return ADODB_oci8::_query($sql,$inputarr);
 	}
+	
+	/**
+	* Replaces compatibility bind markers with oracle ones and returns a
+	* valid sql statement
+	*
+	* This replaces a regexp based section of code that has been subject
+	* to numerous tweaks, as more extreme test cases have appeared. This
+	* is now done this like this to help maintainability and avoid the 
+	* need to rely on regexp experienced maintainers
+   	*
+	* @param	string		$sql		The sql statement
+	* @param	string[]	$inputarr	The bind array
+	*
+	* @return	string	The modified statement
+	*/	
+	final private function extractBinds($sql,$inputarr)
+	{
+		$inString  = false;
+		$escaped   = 0;
+		$sqlLength = strlen($sql) - 1;
+		$newSql    = '';
+		$bindCount = 0;
+		
+		/*
+		* inputarr is the passed in bind list, which is associative, but
+		* we only want the keys here
+		*/
+		$inputKeys = array_keys($inputarr);
+		
+		
+		for ($i=0;$i<=$sqlLength;$i++)
+		{
+			/*
+			* find the next character of the string
+			*/
+			$c = $sql{$i};
+
+			if ($c == "'" && !$inString && $escaped==0)
+				/*
+				* Found the start of a string inside the statement
+				*/
+				$inString = true;
+			elseif ($c == "\\" && $escaped==0)
+				/*
+				* The next character will be escaped
+				*/
+				$escaped = 1;
+			elseif ($c == "'" && $inString && $escaped==0)
+				/*
+				* We found the end of the string
+				*/
+				$inString = false;
+			
+			if ($escaped == 2)
+				$escaped = 0;
+
+			if ($escaped==0 && !$inString && $c == '?')
+				/*
+				* We found a bind symbol, replace it with the oracle equivalent
+				*/
+				$newSql .= ':' . $inputKeys[$bindCount++];
+			else
+				/*
+				* Add the current character the pile
+				*/
+				$newSql .= $c;
+			
+			if ($escaped == 1)
+				/*
+				* We have just found an escape character, make sure we ignore the
+				* next one that comes along, it might be a ' character
+				*/
+				$escaped = 2;
+		}
+		
+		return $newSql;
+			
+	}
 }
 
 /*--------------------------------------------------------------------------------------
