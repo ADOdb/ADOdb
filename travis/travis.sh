a8886170328e6af88388413ee7438bcb2f63cb54
Andy Theuninck <andy@gohanman.com>
2018-08-06 16:55:20 -0500

    Forgot one detail of moving directories

--- travis/travis.sh
+++ travis/travis.sh
@@ -3,4 +3,4 @@
 # I have no earthly idea why travis' YML parser blows up on this line
 # and won't let me put it in the install section
 
-echo '{ "postgres": { "user":"postgres", "password":"" }, "mysql": { "user":"root", "password":"" } }' > tests/credentials.json
+echo '{ "postgres": { "user":"postgres", "password":"" }, "mysql": { "user":"root", "password":"" } }' > travis/credentials.json
