ADOdb/ADOdb

commit 61c5a8cf0ea9df2777eed7d1bb7935593aa93545

Add support for ADODB_FETCH_ASSOC to getMenu()
Methods getMenu and getMenu3 throw E_NOTICE errors when ADODB_FETCH_MODE = ADODB_FETCH_ASSOC See #460. This enhancement adds support for this feature.

adodb-lib.inc.php
@@ -217,6 +217,13 @@ function _adodb_getmenu(&$zthis, $name,$defstr='',$blank1stItem=true,$multiple=f
 			$size=0, $selectAttr='',$compareFields0=true)
 {
 	$hasvalue = false;
+	
+	if (is_array($name))
+	{
+		/*
+		* Reserved for future use
+		*/
+	}
 
 	if ($multiple or is_array($defstr)) {
 		if ($size==0) $size=5;
@@ -227,12 +234,14 @@ function _adodb_getmenu(&$zthis, $name,$defstr='',$blank1stItem=true,$multiple=f
 
 	$s = '<select name="'.$name.'"'.$attr.' '.$selectAttr.'>';
 	if ($blank1stItem)
+	{
 		if (is_string($blank1stItem))  {
 			$barr = explode(':',$blank1stItem);
 			if (sizeof($barr) == 1) $barr[] = '';
 			$s .= "\n<option value=\"".$barr[0]."\">".$barr[1]."</option>";
-		} else $s .= "\n<option></option>";
-
+		} 
+			else $s .= "\n<option></option>";
+	}
 	if ($zthis->FieldCount() > 1) $hasvalue=true;
 	else $compareFields0 = true;
 
@@ -247,35 +256,23 @@ function _adodb_getmenu(&$zthis, $name,$defstr='',$blank1stItem=true,$multiple=f
 			$zthis->MoveNext();
 			continue;
 		}
-
-        if ($fieldsize > 1) {
-			if (isset($zthis->fields[1]))
-				$zval2 = rtrim($zthis->fields[1]);
+		
+		$myFields = array_map('trim',array_values($zthis->fields));
+        
+		if ($fieldsize > 1) {
+			if (isset($myFields[1]))
+				$zval2 = $myFields[1];
 			else
-				$zval2 = rtrim(next($zthis->fields));
+				$zval2 = next($myFields);
 		}
 		$selected = ($compareFields0) ? $zval : $zval2;
 
-        $group = '';
-		if ($fieldsize > 2) {
-            $group = rtrim($zthis->fields[2]);
-        }
-/*
-        if ($optgroup != $group) {
-            $optgroup = $group;
-            if ($firstgroup) {
-                $firstgroup = false;
-                $s .="\n<optgroup label='". htmlspecialchars($group) ."'>";
-            } else {
-                $s .="\n</optgroup>";
-                $s .="\n<optgroup label='". htmlspecialchars($group) ."'>";
-            }
-		}
-*/
+	
 		if ($hasvalue)
 			$value = " value='".htmlspecialchars($zval2)."'";
 
-		if (is_array($defstr))  {
+		if (is_array($defstr))
+		{
 
 			if (in_array($selected,$defstr))
 				$s .= "\n<option selected='selected'$value>".htmlspecialchars($zval).'</option>';
@@ -291,10 +288,7 @@ function _adodb_getmenu(&$zthis, $name,$defstr='',$blank1stItem=true,$multiple=f
 		$zthis->MoveNext();
 	} // while
 
-    // closing last optgroup
-    if($optgroup != null) {
-        $s .= "\n</optgroup>";
-	}
+    
 	return $s ."\n</select>\n";
 }
 
@@ -303,7 +297,14 @@ function _adodb_getmenu_gp(&$zthis, $name,$defstr='',$blank1stItem=true,$multipl
 			$size=0, $selectAttr='',$compareFields0=true)
 {
 	$hasvalue = false;
-
+	
+	if (is_array($name))
+	{
+		/*
+		* Reserved for future use
+		*/
+	}
+	
 	if ($multiple or is_array($defstr)) {
 		if ($size==0) $size=5;
 		$attr = ' multiple size="'.$size.'"';
@@ -334,17 +335,21 @@ function _adodb_getmenu_gp(&$zthis, $name,$defstr='',$blank1stItem=true,$multipl
 			continue;
 		}
 
+		$myFields = array_map('trim',array_values($zthis->fields));
+
         if ($fieldsize > 1) {
-			if (isset($zthis->fields[1]))
-				$zval2 = rtrim($zthis->fields[1]);
+			if (isset($myFields[1]))
+				$zval2 = $myFields[1];
 			else
-				$zval2 = rtrim(next($zthis->fields));
+				$zval2 = next($myFields);
 		}
+		
 		$selected = ($compareFields0) ? $zval : $zval2;
 
         $group = '';
-		if (isset($zthis->fields[2])) {
-            $group = rtrim($zthis->fields[2]);
+		
+		if (isset($myFields[2])) {
+            $group = $myFields[2];
         }
 
         if ($optgroup != $group) {
@@ -383,7 +388,6 @@ function _adodb_getmenu_gp(&$zthis, $name,$defstr='',$blank1stItem=true,$multipl
 	}
 	return $s ."\n</select>\n";
 }
-
 
 /*
 	Count the number of records this sql statement will return by using

commit 44cacdc45d3208fdb3ae349a644b8ea516cf1973

Fix getMenu() for ADODB_FETCH_BOTH
Commit 61c5a8cf0ea9df2777eed7d1bb7935593aa93545 (see #460) introduced a
regression in the behavior of getMenu() when ADODB_FETCH_MODE is set to
ADODB_FETCH_BOTH, causing it to use the recordset's first column for
both the select options' value and description.

Fixes #482

adodb-lib.inc.php
@@ -212,10 +212,11 @@ function _adodb_replace(&$zthis, $table, $fieldArray, $keyCol, $autoQuote, $has_
 		return ($rs) ? 2 : 0;
 }
 
-// Requires $ADODB_FETCH_MODE = ADODB_FETCH_NUM
 function _adodb_getmenu(&$zthis, $name,$defstr='',$blank1stItem=true,$multiple=false,
 			$size=0, $selectAttr='',$compareFields0=true)
 {
+	global $ADODB_FETCH_MODE;
+
 	$hasvalue = false;
 
 	if (is_array($name))
@@ -258,13 +259,15 @@ function _adodb_getmenu(&$zthis, $name,$defstr='',$blank1stItem=true,$multiple=f
 			continue;
 		}
 
-		$myFields = array_map('trim',array_values($zthis->fields));
-
-		if ($fieldsize > 1) {
-			if (isset($myFields[1]))
-				$zval2 = $myFields[1];
-			else
-				$zval2 = next($myFields);
+        if ($fieldsize > 1) {
+			if ($ADODB_FETCH_MODE == ADODB_FETCH_ASSOC) {
+				// Get 2nd field's value regardless of its name
+				$zval2 = current(array_slice($zthis->fields, 1, 1));
+			} else {
+				// With NUM or BOTH fetch modes, we have a numeric index
+				$zval2 = $zthis->fields[1];
+			}
+			$zval2 = trim($zval2);
 		}
 		$selected = ($compareFields0) ? $zval : $zval2;
 
@@ -291,10 +294,11 @@ function _adodb_getmenu(&$zthis, $name,$defstr='',$blank1stItem=true,$multiple=f
 	return $s ."\n</select>\n";
 }
 
-// Requires $ADODB_FETCH_MODE = ADODB_FETCH_NUM
 function _adodb_getmenu_gp(&$zthis, $name,$defstr='',$blank1stItem=true,$multiple=false,
 			$size=0, $selectAttr='',$compareFields0=true)
 {
+	global $ADODB_FETCH_MODE;
+
 	$hasvalue = false;
 
 	if (is_array($name))
@@ -334,13 +338,15 @@ function _adodb_getmenu_gp(&$zthis, $name,$defstr='',$blank1stItem=true,$multipl
 			continue;
 		}
 
-		$myFields = array_map('trim',array_values($zthis->fields));
-
 		if ($fieldsize > 1) {
-			if (isset($myFields[1]))
-				$zval2 = $myFields[1];
-			else
-				$zval2 = next($myFields);
+			if ($ADODB_FETCH_MODE == ADODB_FETCH_ASSOC) {
+				// Get 2nd field's value regardless of its name
+				$zval2 = current(array_slice($zthis->fields, 1, 1));
+			} else {
+				// With NUM or BOTH fetch modes, we have a numeric index
+				$zval2 = $zthis->fields[1];
+			}
+			$zval2 = trim($zval2);
 		}
 
 		$selected = ($compareFields0) ? $zval : $zval2;