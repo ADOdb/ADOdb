60938a6407dfdab6d7d23868dc2a6ca479019aab
Damien Regad <dregad@mantisbt.org>
2019-11-28 19:31:37 +0100

    Add file-level PHPdoc block with copyright

--- drivers/adodb-pdo_dblib.inc.php
+++ drivers/adodb-pdo_dblib.inc.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * ADOdb PDO dblib driver.
+ *
+ * Released under both BSD license and Lesser GPL library license.
+ * Whenever there is any discrepancy between the two licenses, the BSD license
+ * will take precedence.
+ *
+ * @version   v5.21.0-dev  ??-???-2016
+ * @copyright (c) 2000-2013 John Lim (jlim#natsoft.com). All rights reserved.
+ * @copyright (c) 2019      Damien Regad, Mark Newnham and the ADOdb community
+ */
 
 class ADODB_pdo_dblib extends ADODB_pdo
 {
