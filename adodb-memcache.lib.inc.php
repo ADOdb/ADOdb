cc28aa266d1614b1418c9c29bb3dec18b7f0c17b
Mark Newnham <mark@newnhams.com>
2019-12-21 14:45:38 -0700

    Added opt feature for mmcached

--- adodb-memcache.lib.inc.php
+++ adodb-memcache.lib.inc.php
@@ -3,8 +3,8 @@
 // security - hide paths
 if (!defined('ADODB_DIR')) die();
 
-global $ADODB_INCLUDED_MEMCACHE;
-$ADODB_INCLUDED_MEMCACHE = 1;
+global $ADODB_INCLUDEDmemcacheLibrary;
+$ADODB_INCLUDEDmemcacheLibrary = 1;
 
 /*
 
@@ -23,7 +23,7 @@ $ADODB_INCLUDED_MEMCACHE = 1;
   Class instance is stored in $ADODB_CACHE
 */
 
-class ADODB_Cache_MemCache 
+class ADODB_CachememcacheLibrary 
 {
 	/*
 	* Prevents parent class calling non-existant function
@@ -31,11 +31,11 @@ class ADODB_Cache_MemCache
 	public $createdir = false; 
 
 	/*
-	* $library will be populated with the proper library on connect
+	* populated with the proper library on connect
 	* and is used later when there are differences in specific calls
 	* between memcache and memcached
 	*/
-	private $library = false;
+	private $memCacheLibrary = false;
 	
 	/*
 	* array of hosts
@@ -52,17 +52,57 @@ class ADODB_Cache_MemCache
 	*/
 	private $compress = false; 
 
+	/*
+	* Array of options for memcached only
+	*/
+	private $options = false;
 	
 	/*
-	* Internal flag indicating connection
+	* Internal flag indicating successful connection
 	*/
-	private $_connected = false;
+	private $isConnected = false;
 	
 	/*
 	* Handle for the Memcache library
 	*/
-	private $_memcache = false;
+	private $memcacheLibrary = false
+	
+	/*
+	* New server feature controller lists available servers
+	*/
+	private $serverControllers = array();
+	
+	/*
+	* New server feature controller uses granular
+	* server controller
+	*/
+	private $serverControllerTemplate = array(
+		'host'=>'',
+		'port'=>11211,
+		'weight'=>0,
+		'key'=>''
+		);
 
+	
+	/*
+	* An integer index into the libraries
+	*/
+	const MCLIB  = 1;
+	const MCLIBD = 2;
+	
+	/*
+	* Xrefs the library flag to the actual class name
+	*/
+	private $libraries = array(
+		1=>'Memcache',
+		2=>'Memcached'
+		);
+	
+	/*
+	* An indicator of which library we are using
+	*/
+	private $libraryFlag;
+	
 	/**
 	* constructor passes in a ADONewConnection Object
 	*
@@ -75,6 +115,8 @@ class ADODB_Cache_MemCache
 		$this->hosts 	= $db->memCacheHost;
 		$this->port  	= $db->memCachePort;
 		$this->compress = $db->memCacheCompress;
+		$this->options  = $db->memCacheOptions;
+		
 	}
 
 	/**
@@ -90,33 +132,133 @@ class ADODB_Cache_MemCache
 		* do we have memcache or memcached? see the note
 		* at adodb.org on memcache
 		*/
-		if (class_exists('Memcache')) {
-			$this->library='Memcache';
-			$memcache = new MemCache;
-		} elseif (class_exists('Memcached')) {
-			$this->library='Memcached';
-			$memcache = new MemCached;
-		} else {
+		if (class_exists('Memcache')) 
+			$this->libraryFlag = self::MCLIB;
+		elseif (class_exists('Memcached'))
+			$this->libraryFlag = self::MCLIB;
+		else
+		{
 			$err = 'Neither the Memcache nor Memcached PECL extensions were found!';
 			return false;
 		}
-
-		if (!is_array($this->hosts)) 
-			$this->hosts = array($this->hosts);
-
-		$failcnt = 0;
-		foreach($this->hosts as $host) 
+		
+		$this->memCacheLibrary = new {$this->libraries[$libraryFlag]};
+		if (!$this->memcacheLibrary)
 		{
-			if (!@$memcache->addServer($host,$this->port)) {
-				$failcnt += 1;
-			}
-		}
-		if ($failcnt == sizeof($this->hosts)) {
-			$err = 'Can\'t connect to any memcache server';
+			$err = 'Memcache library failed to initialize';
 			return false;
 		}
-		$this->_connected = true;
-		$this->_memcache = $memcache;
+		
+		/*
+		* Convert simple compression flag for memcached
+		*/
+		if ($this->libraryFlag == self::MCLIBD && $this->compress)
+		{
+			/*
+			* Value of Memcached::OPT_COMPRESSION = 2;
+			*/
+			$this->options[2] == 1
+		}
+		/*
+		* Are there any options available for memcached
+		*/
+		if ($this->libraryFlag == self::MCLIBD && count($this->options) > 0)
+		{
+			$optionSuccess = $this->memCacheLibrary->setOptions($this->options);
+			if (!$optionSuccess)
+			{
+				$err = 'Invalid option parameters passed to Memecached';
+				return false;
+			}
+		}
+		
+		/*
+		* Have we passed a controller array
+		*/
+		if (!is_array($this->hosts)) 
+			$this->hosts = array($this->hosts);
+		
+		if (array_values($this->hosts) == $this->hosts)
+		{
+			/*
+			* Old way, convert to controller
+			*/
+			foreach ($this->hosts as $ipAddress)
+			{
+				$connector = $this->serverControllerTemplate;
+				
+				$connector['host'] = $ipAddress;
+				$connector['port'] = $this->port;
+				
+				$this->serverControllers[] = $connector;
+			}
+		}
+		else
+		{
+			/*
+			* New way, must validate port, etc
+			*/
+			foreach ($this->hosts as $controller)
+			{
+				$connector = array_merge($this->serverControllerTemplate,$controller);
+				if ($this->libraryFlag == self::MCLIB)
+				{
+					/*
+					* Cannot use a key or weight in memcache, simply discard
+					*/
+					$connector['key'] = '';
+					$connector['weight'] = 0;
+					
+				}
+				else
+					$connector['weight'] = $connector['weight'] ? (int)$connector['weight']:0;
+				
+				$this->serverControllers[] = $connector;
+			}
+		}
+
+		/*
+		* Checks for existing connections ( but only for memcached )
+		*/
+		if ($this->libraryFlag == self::MCLIBD)
+		{
+			$existingServers = $memCache->getServerList();
+			if (is_array($existingServers))
+			{
+				/*
+				* Use the existing configuration
+				*/
+				$this->isConnected = true;
+				$this->memcacheLibrary = $memcache;
+				return true;
+			}
+		}
+		$failcnt = 0;
+		foreach($this->serverControllers as $controller) 
+		{
+			switch($this->libraryFlag)
+			{
+				case self::MCLIB:
+				if (!@$this->memcacheLibrary->addServer($controller['host'],$controller['port']))
+					$failcnt++;
+				break;
+				default:
+				if (!@$this->memcacheLibrary->addServer($controller['host'],$controller['port'],$controller['weight']))
+					$failcnt++;
+			}
+			
+		}
+		if ($failcnt == sizeof($this->serverControllers))
+		{
+			$err = 'Can\'t connect to any memcache server';
+			return false;
+
+		}
+		
+		/*
+		* A valid memcache connection is available
+		*/
+		$this->isConnected = true;
 		return true;
 	}
 
@@ -132,21 +274,32 @@ class ADODB_Cache_MemCache
 	*/
 	public function writeCache($filename, $contents, $debug, $secs2cache)
 	{
-		if (!$this->_connected) {
-			$err = '';
-			if (!$this->connect($err) && $debug) ADOConnection::outp($err);
+		$err = '';
+		if (!$this->isConnected  && $debug) 
+		{
+			/*
+			* Call to writecache before connect, try
+			* to connect
+			*/
+			if (!$this->connect($err)) 
+				ADOConnection::outp($err);
 		}
-		if (!$this->_memcache) return false;
+		else if (!$this->isConnected) 
+			$this->connect($err);
+		
+		if (!$this->memcacheLibrary) 
+			return false;
 
 		$failed=false;
-		switch ($this->library) {
-			case 'Memcache':
-				if (!$this->_memcache->set($filename, $contents, $this->compress ? MEMCACHE_COMPRESSED : 0, $secs2cache)) {
+		switch ($this->libraryFlag)
+		{
+			case self::MCLIB:
+				if (!$this->memcacheLibrary->set($filename, $contents, $this->compress ? MEMCACHE_COMPRESSED : 0, $secs2cache)) {
 					$failed=true;
 				}
 				break;
-			case 'Memcached':
-				if (!$this->_memcache->set($filename, $contents, $secs2cache)) {
+			case self::MCLIBD:
+				if (!$this->memcacheLibrary->set($filename, $contents, $secs2cache)) {
 					$failed=true;
 				}
 				break;
@@ -175,11 +328,11 @@ class ADODB_Cache_MemCache
 	*/
 	public function readCache($filename, &$err, $secs2cache, $rsClass)
 	{
-		if (!$this->_connected) $this->connect($err);
-		if (!$this->_memcache) 
+		if (!$this->isConnected) $this->connect($err);
+		if (!$this->memcacheLibrary) 
 			return false;
 
-		$rs = $this->_memcache->get($filename);
+		$rs = $this->memcacheLibrary->get($filename);
 		if (!$rs) 
 		{
 			$err = 'Item with such key doesn\'t exist on the memcache server.';
@@ -232,14 +385,14 @@ class ADODB_Cache_MemCache
 	*/
 	public function flushAll($debug=false)
 	{
-		if (!$this->_connected) {
+		if (!$this->isConnected) {
 			$err = '';
 			if (!$this->connect($err) && $debug) ADOConnection::outp($err);
 		}
-		if (!$this->_memcache) 
+		if (!$this->memcacheLibrary) 
 			return false;
 
-		$del = $this->_memcache->flush();
+		$del = $this->memcacheLibrary->flush();
 
 		if ($debug)
 			if (!$del) 
@@ -260,15 +413,15 @@ class ADODB_Cache_MemCache
 	*/
 	public function flushCache($filename, $debug=false)
 	{
-		if (!$this->_connected) 
+		if (!$this->isConnected) 
 		{
 			$err = '';
 			if (!$this->connect($err) && $debug) ADOConnection::outp($err);
 		}
-		if (!$this->_memcache) 
+		if (!$this->memcacheLibrary) 
 			return false;
 
-		$del = $this->_memcache->delete($filename);
+		$del = $this->memcacheLibrary->delete($filename);
 
 		if ($debug)
 			if (!$del) ADOConnection::outp("flushcache: $key entry doesn't exist on memcache server!<br>\n");
