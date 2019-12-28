a00650552e0c3c63a05e07859ad9d25cf607dd61
Mark Newnham <mark@newnhams.com>
2019-12-28 11:16:50 -0700

    Added MetaColumns Function

--- drivers/adodb-pdo_sqlsrv.inc.php
+++ drivers/adodb-pdo_sqlsrv.inc.php
@@ -11,7 +11,28 @@ class ADODB_pdo_sqlsrv extends ADODB_pdo
 	
 	public $metaTablesSQL="select name,case when type='U' then 'T' else 'V' end from sysobjects where (type='U' or type='V') and (name not in ('sysallocations','syscolumns','syscomments','sysdepends','sysfilegroups','sysfiles','sysfiles1','sysforeignkeys','sysfulltextcatalogs','sysindexes','sysindexkeys','sysmembers','sysobjects','syspermissions','sysprotects','sysreferences','systypes','sysusers','sysalternates','sysconstraints','syssegments','REFERENTIAL_CONSTRAINTS','CHECK_CONSTRAINTS','CONSTRAINT_TABLE_USAGE','CONSTRAINT_COLUMN_USAGE','VIEWS','VIEW_TABLE_USAGE','VIEW_COLUMN_USAGE','SCHEMATA','TABLES','TABLE_CONSTRAINTS','TABLE_PRIVILEGES','COLUMNS','COLUMN_DOMAIN_USAGE','COLUMN_PRIVILEGES','DOMAINS','DOMAIN_CONSTRAINTS','KEY_COLUMN_USAGE','dtproperties'))";
 
+	public $metaColumnsSQL =
+		"select c.name,
+		t.name as type,
+		c.length,
+		c.xprec as precision,
+		c.xscale as scale,
+		c.isnullable as nullable,
+		c.cdefault as default_value,
+		c.xtype,
+		t.length as type_length,
+		sc.is_identity
+		from syscolumns c
+		join systypes t on t.xusertype=c.xusertype
+		join sysobjects o on o.id=c.id
+		join sys.tables st on st.name=o.name
+		join sys.columns sc on sc.object_id = st.object_id and sc.name=c.name
+		where o.name='%s'";
+
 	var $arrayClass = 'ADORecordSet_array_pdo_sqlsrv';
+	
+	public $cachedSchemaFlush = false;
+
 
 	function _init(ADODB_pdo $parentDriver)
 	{
@@ -30,7 +51,85 @@ class ADODB_pdo_sqlsrv extends ADODB_pdo
 		return $returnval;
 	}
 
-	function MetaColumns($table, $normalize = true)
+	function metaColumns($table, $upper=true, $schema=false){
+
+		/*
+		* A simple caching mechanism, to be replaced in ADOdb V6
+		*/
+		static $cached_columns = array();
+		if ($this->cachedSchemaFlush)
+			$cached_columns = array();
+
+		if (array_key_exists($table,$cached_columns)){
+			return $cached_columns[$table];
+		}
+
+
+		$this->_findschema($table,$schema);
+		if ($schema) {
+			$dbName = $this->database;
+			$this->SelectDB($schema);
+		}
+		global $ADODB_FETCH_MODE;
+		$save = $ADODB_FETCH_MODE;
+		$ADODB_FETCH_MODE = ADODB_FETCH_NUM;
+
+		if ($this->fetchMode !== false) $savem = $this->SetFetchMode(false);
+		$rs = $this->Execute(sprintf($this->metaColumnsSQL,$table));
+
+		if ($schema) {
+			$this->SelectDB($dbName);
+		}
+
+		if (isset($savem)) $this->SetFetchMode($savem);
+		$ADODB_FETCH_MODE = $save;
+		if (!is_object($rs)) {
+			$false = false;
+			return $false;
+		}
+
+		$retarr = array();
+		while (!$rs->EOF){
+
+			$fld = new ADOFieldObject();
+			if (array_key_exists(0,$rs->fields)) {
+				$fld->name          = $rs->fields[0];
+				$fld->type          = $rs->fields[1];
+				$fld->max_length    = $rs->fields[2];
+				$fld->precision     = $rs->fields[3];
+				$fld->scale         = $rs->fields[4];
+				$fld->not_null      =!$rs->fields[5];
+				$fld->has_default   = $rs->fields[6];
+				$fld->xtype         = $rs->fields[7];
+				$fld->type_length   = $rs->fields[8];
+				$fld->auto_increment= $rs->fields[9];
+			} else {
+				$fld->name          = $rs->fields['name'];
+				$fld->type          = $rs->fields['type'];
+				$fld->max_length    = $rs->fields['length'];
+				$fld->precision     = $rs->fields['precision'];
+				$fld->scale         = $rs->fields['scale'];
+				$fld->not_null      =!$rs->fields['nullable'];
+				$fld->has_default   = $rs->fields['default_value'];
+				$fld->xtype         = $rs->fields['xtype'];
+				$fld->type_length   = $rs->fields['type_length'];
+				$fld->auto_increment= $rs->fields['is_identity'];
+			}
+
+			if ($save == ADODB_FETCH_NUM)
+				$retarr[] = $fld;
+			else
+				$retarr[strtoupper($fld->name)] = $fld;
+
+			$rs->MoveNext();
+
+		}
+		$rs->Close();
+		$cached_columns[$table] = $retarr;
+
+		return $retarr;
+	}
+	function xMetaColumns($table, $normalize = true)
 	{
 		return false;
 	}
