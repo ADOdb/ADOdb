f9f395672e8b03657cc3c67ef2a122eb2fd81abc
Mark Newnham <mark@newnhams.com>
2020-03-30 19:25:06 -0600

    PHP7.4 throws error at uninitialized array, #608
    method Fields() accessed an unitialized array, that was not
    trapped in versions of PHP prior to 7.4. method now checks
    to see if array is initialized.

--- drivers/adodb-mssqlnative.inc.php
+++ drivers/adodb-mssqlnative.inc.php
@@ -1061,7 +1061,14 @@ class ADORecordset_mssqlnative extends ADORecordSet {
 	/* Use associative array to get fields array */
 	function Fields($colname)
 	{
-		if ($this->fetchMode != ADODB_FETCH_NUM) return $this->fields[$colname];
+		if (!is_array($this->fields))
+			/*
+			* Too early
+			*/
+			return;
+		if ($this->fetchMode != ADODB_FETCH_NUM) 
+			return $this->fields[$colname];
+		
 		if (!$this->bind) {
 			$this->bind = array();
 			for ($i=0; $i < $this->_numOfFields; $i++) {
