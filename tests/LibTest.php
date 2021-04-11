270ad43af56a12445ed100a6c2ebeed38372f1ed
Damien Regad <dregad@mantisbt.org>
2021-04-11 17:46:18 +0200

    Test case for _adodb_quote_fieldname()
    This is a basic test case to ensure the code matches expected results
    as discussed in #721 [[1]].
    
    [1]: https://github.com/ADOdb/ADOdb/issues/721#issuecomment-817338903

---
+++ tests/LibTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once dirname(__FILE__) . '/../adodb.inc.php';
+require_once dirname(__FILE__) . '/../adodb-lib.inc.php';
+
+/**
+ * Class LibTest.
+ *
+ * Test cases for adodb-lib.inc.php
+ */
+class LibTest extends TestCase
+{
+	/** @var ADOConnection Database connection for tests */
+	private $db;
+
+	public function setUp()
+	{
+		$this->db = ADONewConnection('mysqli');
+	}
+
+	/**
+	 * Test for {@see _adodb_quote_fieldname()}
+	 *
+	 * @dataProvider quoteProvider
+	 */
+	public function testQuoteFieldNames($method, $field, $expected)
+	{
+		global $ADODB_QUOTE_FIELDNAMES;
+		$ADODB_QUOTE_FIELDNAMES = $method;
+		$this->assertSame($expected, _adodb_quote_fieldname($this->db, $field));
+	}
+
+	/**
+	 * Data provider for {@see testQuoteFieldNames()}
+	 * @return array
+	 */
+	public function quoteProvider()
+	{
+		return [
+			'No quoting, single-word field name' => [false, 'Field', 'FIELD'],
+			'No quoting, field name with space' => [false, 'Field Name', '`FIELD NAME`'],
+			'Quoting `true`' => [true, 'Field', '`FIELD`'],
+			'Quoting `UPPER`' => ['UPPER', 'Field', '`FIELD`'],
+			'Quoting `LOWER`' => ['LOWER', 'Field', '`field`'],
+			'Quoting `NATIVE`' => ['NATIVE', 'Field', '`Field`'],
+			'Quoting `BRACKETS`' => ['BRACKETS', 'Field', '[FIELD]'],
+			'Unknown value defaults to UPPER' => ['XXX', 'Field', '`FIELD`'],
+		];
+	}
+
+}
