af9234a525c3255af051a330164486d73be4c63a
Damien Regad <dregad@mantisbt.org>
2021-08-16 00:53:57 +0200

    Reset version to 5.22.0-dev
    It was inadvertantly changed when merging the standardized headers
    (e9dcce3df24912ad869d0193f0b419f2309101fc).

--- adodb.inc.php
+++ adodb.inc.php
@@ -198,7 +198,7 @@ if (!defined('_ADODB_LAYER')) {
 		/**
 		 * ADODB version as a string.
 		 */
-		$ADODB_vers = 'v5.21.1  2021-08-15';
+		$ADODB_vers = 'v5.22.0-dev  Unreleased';
 
 		/**
 		 * Determines whether recordset->RecordCount() is used.
