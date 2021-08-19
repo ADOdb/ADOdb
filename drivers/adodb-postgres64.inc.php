ADOdb/ADOdb

commit 9f6f327b9d5b29d14565fa0ab4308354bf050d2d

PHPDoc, code style

drivers/adodb-postgres64.inc.php
@@ -951,10 +951,9 @@ class ADORecordSet_postgres64 extends ADORecordSet{
 		return $row;
 	}
 
-
-	function _initrs()
+	function _initRS()
 	{
-	global $ADODB_COUNTRECS;
+		global $ADODB_COUNTRECS;
 		$qid = $this->_queryID;
 		$this->_numOfRows = ($ADODB_COUNTRECS)? @pg_num_rows($qid):-1;
 		$this->_numOfFields = @pg_num_fields($qid);
@@ -969,10 +968,11 @@ class ADORecordSet_postgres64 extends ADORecordSet{
 		}
 	}
 
-		/* Use associative array to get fields array */
-	function Fields($colname)
+	function fields($colname)
 	{
-		if ($this->fetchMode != PGSQL_NUM) return @$this->fields[$colname];
+		if ($this->fetchMode != PGSQL_NUM) {
+			return @$this->fields[$colname];
+		}
 
 		if (!$this->bind) {
 			$this->bind = array();
@@ -984,7 +984,7 @@ class ADORecordSet_postgres64 extends ADORecordSet{
 		return $this->fields[$this->bind[strtoupper($colname)]];
 	}
 
-	function FetchField($off = 0)
+	function fetchField($fieldOffset)
 	{
 		// offsets begin at 0
 

adodb.inc.php
@@ -3620,7 +3620,8 @@ http://www.stanford.edu/dept/itss/docs/oracle/10g/server.101/b10759/statements_1
 	 */
 	var $_numOfRows = -1;	/** number of rows, or -1 */
 	var $_numOfFields = -1;	/** number of fields in recordset */
-	var $_queryID = -1;		/** This variable keeps the result link identifier.	*/
+	/** @var resource result link identifier */
+	var $_queryID = -1;
 	var $_currentRow = -1;	/** This variable keeps the current row in the Recordset.	*/
 	var $_closed = false;	/** has recordset been closed */
 	var $_inited = false;	/** Init() should only be called once */
@@ -4444,21 +4445,23 @@ http://www.stanford.edu/dept/itss/docs/oracle/10g/server.101/b10759/statements_1
 	}
 
 	/**
-	 * Get the ADOFieldObject of a specific column.
+	 * Get a Field's metadata from database.
 	 *
-	 * @param fieldoffset	is the column position to access(0-based).
+	 * Must be defined by child class.
 	 *
-	 * @return the ADOFieldObject for that column, or false.
+	 * @param int $fieldOffset
+	 *
+	 * @return ADOFieldObject|false
 	 */
-	function FetchField($fieldoffset = -1) {
-		// must be defined by child class
-
+	function fetchField($fieldOffset)
+	{
 		return false;
 	}
 
 	/**
-	 * Get the ADOFieldObjects of all columns in an array.
+	 * Get Field metadata for all the recordset's columns in an array.
 	 *
+	 * @return ADOFieldObject[]
 	 */
 	function fieldTypesArray() {
 		if (empty($this->fieldObjectsCache)) {

drivers/adodb-mssqlnative.inc.php
@@ -1142,7 +1142,7 @@ class ADORecordset_mssqlnative extends ADORecordSet {
 	* the next field that wasn't yet retrieved by fetchField()
 	* is retrieved.
 	*
-	* $param int $fieldOffset (optional default=-1 for all
+	* @param int $fieldOffset (optional default=-1 for all
 	* @return mixed an ADOFieldObject, or array of objects
 	*/
 	private function _fetchField($fieldOffset = -1)

commit 55de14348c0ed1c7994dec7f10c8db0b8aee68e1

pgsql: fix fetchField() parameter naming
Regression from 9f6f327b9d5b29d14565fa0ab4308354bf050d2d.

fetchField()'s parameter was renamed from $off to $fieldOffset, but the
change was not reflected in the method's body.

Fixes #752

drivers/adodb-postgres64.inc.php
@@ -984,14 +984,14 @@ class ADORecordSet_postgres64 extends ADORecordSet{
 		return $this->fields[$this->bind[strtoupper($colname)]];
 	}
 
-	function fetchField($fieldOffset)
+	function fetchField($fieldOffset = 0)
 	{
 		// offsets begin at 0
 
-		$o= new ADOFieldObject();
-		$o->name = @pg_field_name($this->_queryID,$off);
-		$o->type = @pg_field_type($this->_queryID,$off);
-		$o->max_length = @pg_field_size($this->_queryID,$off);
+		$o = new ADOFieldObject();
+		$o->name = @pg_field_name($this->_queryID, $fieldOffset);
+		$o->type = @pg_field_type($this->_queryID, $fieldOffset);
+		$o->max_length = @pg_field_size($this->_queryID, $fieldOffset);
 		return $o;
 	}
 