95b5cdf7d3405e491c3ebe411bdb6ef9a08651bd
Mark Newnham <mark@newnhams.com>
2021-12-05 15:18:53 -0700

    Add support for setFieldsObjectCache to mysqli driver

--- drivers/adodb-mysqli.inc.php
+++ drivers/adodb-mysqli.inc.php
@@ -1434,71 +1434,109 @@ class ADORecordSet_mysqli extends ADORecordSet{
 		parent::__construct($queryID);
 	}
 
+	/**
+	 * Initializes the recordset
+	 *
+	 * @return void
+	 */
 	function _initrs()
 	{
-	global $ADODB_COUNTRECS;
+		global $ADODB_COUNTRECS;
 
 		$this->_numOfRows = $ADODB_COUNTRECS ? @mysqli_num_rows($this->_queryID) : -1;
 		$this->_numOfFields = @mysqli_num_fields($this->_queryID);
+		$this->setFieldObjectsCache();
 	}
 
-/*
-1      = MYSQLI_NOT_NULL_FLAG
-2      = MYSQLI_PRI_KEY_FLAG
-4      = MYSQLI_UNIQUE_KEY_FLAG
-8      = MYSQLI_MULTIPLE_KEY_FLAG
-16     = MYSQLI_BLOB_FLAG
-32     = MYSQLI_UNSIGNED_FLAG
-64     = MYSQLI_ZEROFILL_FLAG
-128    = MYSQLI_BINARY_FLAG
-256    = MYSQLI_ENUM_FLAG
-512    = MYSQLI_AUTO_INCREMENT_FLAG
-1024   = MYSQLI_TIMESTAMP_FLAG
-2048   = MYSQLI_SET_FLAG
-32768  = MYSQLI_NUM_FLAG
-16384  = MYSQLI_PART_KEY_FLAG
-32768  = MYSQLI_GROUP_FLAG
-65536  = MYSQLI_UNIQUE_FLAG
-131072 = MYSQLI_BINCMP_FLAG
-*/
+	/**
+	 * Get column information in the Recordset object.
+	 * fetchField() can be used in order to obtain information about fields
+	 * in a certain query result. If the field offset isn't specified, the next
+	 * field that wasn't yet retrieved by fetchField() is retrieved
+	 *
+	 * @param int		$fieldOffset
+	 * @return object 	containing field information
+	 */
+	protected function setFieldObjectsCache($fieldOffset = -1)
+	{
+		
+		if ($this->fieldObjectsRetrieved) {
+			if ($this->fieldObjectsCache) {
+				// Already got the information
+				if ($fieldOffset == -1) {
+					return $this->fieldObjectsCache;
+				} else {
+					return $this->fieldObjectsCache[$fieldOffset];
+				}
+			} else {
+				// No metadata available
+				return false;
+			}
+		}
+
+		$this->fieldObjectsCache = array();
+		
+		$this->fieldObjectsRetrieved = true;
+		/*
+		 * Retrieve all metadata in one go. This is always returned as a
+		 * numeric array.
+		 */
+		$fieldMetaData = @mysqli_fetch_fields($this->_queryID);
+
+		if (!$fieldMetaData) {
+			/*
+			* Not a statement that gives us metaData
+			*/
+			return false;
+		}
+
+		foreach ($fieldMetaData as $o)
+		{
+			
+			/*
+			* Fix for HHVM
+			*/ 
+			if ( !isset($o->flags) )
+			{
+				$o->flags = 0;
+			}
+						
+			/* 
+			* Properties of an ADOFieldObject as set by MetaColumns
+			* @link https://docs.oracle.com/cd/E17952_01/apis-php-en/apis-php-mysqli.constants.html
+			*/
+			$o->primary_key 	= $o->flags & MYSQLI_PRI_KEY_FLAG;
+			$o->not_null 		= $o->flags & MYSQLI_NOT_NULL_FLAG;
+			$o->auto_increment 	= $o->flags & MYSQLI_AUTO_INCREMENT_FLAG;
+			$o->binary 			= $o->flags & MYSQLI_BINARY_FLAG;
+			// $o->blob 		= $o->flags & MYSQLI_BLOB_FLAG; /* not returned by MetaColumns */
+			$o->unsigned 		= $o->flags & MYSQLI_UNSIGNED_FLAG;
+
+			/*
+			* Trivial method to cast mysql_result object to ADOfieldObject
+			*/
+			$a = new ADOFieldObject;
+			foreach (get_object_vars($o) as $key => $name)
+				$a->$key = $name;
+		
+			$this->fieldObjectsCache[] = $o;
+
+		}
+	}
 
 	/**
-	 * Returns raw, database specific information about a field.
-	 *
+	 * Returns the metadata for a specific field
+	 * 
 	 * @link https://adodb.org/dokuwiki/doku.php?id=v5:reference:recordset:fetchfield
-	 *
-	 * @param int $fieldOffset (Optional) The field number to get information for.
-	 *
-	 * @return ADOFieldObject|bool
+	 * 
+	 * @param integer $fieldOffset
+	 * 
+	 * @return bool|ADOFieldObject
 	 */
-	function FetchField($fieldOffset = -1)
+	function fetchField($fieldOffset = -1)
 	{
-		$fieldnr = $fieldOffset;
-		if ($fieldOffset != -1) {
-			$fieldOffset = @mysqli_field_seek($this->_queryID, $fieldnr);
-		}
-		$o = @mysqli_fetch_field($this->_queryID);
-		if (!$o) return false;
-
-		//Fix for HHVM
-		if ( !isset($o->flags) ) {
-			$o->flags = 0;
-		}
-		/* Properties of an ADOFieldObject as set by MetaColumns */
-		$o->primary_key = $o->flags & MYSQLI_PRI_KEY_FLAG;
-		$o->not_null = $o->flags & MYSQLI_NOT_NULL_FLAG;
-		$o->auto_increment = $o->flags & MYSQLI_AUTO_INCREMENT_FLAG;
-		$o->binary = $o->flags & MYSQLI_BINARY_FLAG;
-		// $o->blob = $o->flags & MYSQLI_BLOB_FLAG; /* not returned by MetaColumns */
-		$o->unsigned = $o->flags & MYSQLI_UNSIGNED_FLAG;
-
-		/*
-		* Trivial method to cast class to ADOfieldObject
-		*/
-		$a = new ADOFieldObject;
-		foreach (get_object_vars($o) as $key => $name)
-			$a->$key = $name;
-		return $a;
+		if (array_key_exists($fieldOffset,$this->fieldObjectsCache))
+			return $this->fieldObjectsCache[$fieldOffset];
 	}
 
 	/**
