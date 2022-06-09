ADOdb/ADOdb

commit 7b0d040355753aa95e6068b54bbaf3014f8da3e8

Fix native mode casing issue, see #610

adodb-active-record.inc.php
@@ -496,7 +496,6 @@ class ADODB_Active_Record {
 			break;
 		default:
 			foreach($cols as $name => $fldobj) {
-				$name = ($fldobj->name);
 
 				if ($ADODB_ACTIVE_DEFVALS && isset($fldobj->default_value)) {
 					$this->$name = $fldobj->default_value;
@@ -507,7 +506,7 @@ class ADODB_Active_Record {
 				$attr[$name] = $fldobj;
 			}
 			foreach($pkeys as $k => $name) {
-				$keys[$name] = $cols[$name]->name;
+				$keys[$name] = $cols[strtoupper($name)]->name;
 			}
 			break;
 		}

commit 996d27ad7ee5b7a80fb1a3ad7732064a1e17e9fe

Fix Active Record changing case of properties
With ADODB_ASSOC_CASE and ADODB_QUOTE_FIELDNAMES in Native mode, active
record class properties were converted to uppercase.

Problem was introduced by commit 7b0d040355753aa95e6068b54bbaf3014f8da3e8
(see #610).

Fixes #837

adodb-active-record.inc.php
@@ -501,7 +501,8 @@ class ADODB_Active_Record {
 			}
 			break;
 		default:
-			foreach($cols as $name => $fldobj) {
+			foreach($cols as $fldobj) {
+				$name = $fldobj->name;
 
 				if ($ADODB_ACTIVE_DEFVALS && isset($fldobj->default_value)) {
 					$this->$name = $fldobj->default_value;