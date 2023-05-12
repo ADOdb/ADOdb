ADOdb/ADOdb

commit bda6e1ea4271d0818dc22213e6a37269eea427b4

Fix PHP 8.1 deprecated warning in tohtml.inc.php
Calling rs2html() with a recordset containing a NULL value results in a
deprecated warning on PHP 8.1.

Fixes #850

tohtml.inc.php
@@ -139,11 +139,14 @@ GLOBAL $gSQLMaxRows,$gSQLBlockRows,$ADODB_ROUND;
 			*/
 
 			default:
-				if ($htmlspecialchars) $v = htmlspecialchars(trim($v));
-				$v = trim($v);
-				if (strlen($v) == 0) $v = '&nbsp;';
-				$s .= "	<TD>". str_replace("\n",'<br>',stripslashes($v)) ."</TD>\n";
-
+				if ($v) {
+					$v = htmlspecialchars(stripslashes(trim($v)));
+				} elseif ($v === null) {
+					$v = '(NULL)';
+				} else {
+					$v = '&nbsp;';
+				}
+				$s .= "	<TD>" . str_replace("\n", '<br>', $v) . "</TD>\n";
 			}
 		} // for
 		$s .= "</TR>\n\n";

commit 93fa6785ad0c4f51f44d0e5a9055575e4337742b

Restore rs2html() $htmlspecialchars param behavior
Regression introduced by bda6e1ea4271d0818dc22213e6a37269eea427b4.

Fixes #968, PR #969

Signed-off-by: Damien Regad <dregad@mantisbt.org>

Changes from original contribution:
- extract trim() call from if statement
- revised commit message
- backport to hotfix/5.22 branch

tohtml.inc.php
@@ -141,7 +141,10 @@ GLOBAL $gSQLMaxRows,$gSQLBlockRows,$ADODB_ROUND;
 
 			default:
 				if ($v) {
-					$v = htmlspecialchars(stripslashes(trim($v)));
+					$v = trim($v);
+					if ($htmlspecialchars) {
+						$v = htmlspecialchars($v);
+					}
 				} elseif ($v === null) {
 					$v = '(NULL)';
 				} else {