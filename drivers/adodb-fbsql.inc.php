ADOdb/ADOdb

commit 5c9e24cf98409004da2a5e5bd80eeec82c5a3997

Set $fetchMode property in ADORecordSet constructor
This reduces code duplication as the logic to initialize $fetchMode from
$ADODB_FETCH_MODE global was repeated in most drivers.

$adodbFetchMode is systematically initialized as well now; previously it
was only set in those drivers having specific fetch modes.

Fixes #958

drivers/adodb-fbsql.inc.php
@@ -169,20 +169,22 @@ class ADORecordSet_fbsql extends ADORecordSet{
 	var $databaseType = "fbsql";
 	var $canSeek = true;
 
-	function __construct($queryID,$mode=false)
+	function __construct($queryID, $mode=false)
 	{
-		if (!$mode) {
-			global $ADODB_FETCH_MODE;
-			$mode = $ADODB_FETCH_MODE;
-		}
+		parent::__construct($queryID, $mode);
+
 		switch ($mode) {
-		case ADODB_FETCH_NUM: $this->fetchMode = FBSQL_NUM; break;
-		case ADODB_FETCH_ASSOC: $this->fetchMode = FBSQL_ASSOC; break;
-		case ADODB_FETCH_BOTH:
-		default:
-		$this->fetchMode = FBSQL_BOTH; break;
+			case ADODB_FETCH_NUM:
+				$this->fetchMode = FBSQL_NUM;
+				break;
+			case ADODB_FETCH_ASSOC:
+				$this->fetchMode = FBSQL_ASSOC;
+				break;
+			case ADODB_FETCH_BOTH:
+			default:
+				$this->fetchMode = FBSQL_BOTH;
+				break;
 		}
-		parent::__construct($queryID);
 	}
 
 	function _initrs()

drivers/adodb-ldap.inc.php
@@ -295,28 +295,23 @@ class ADORecordSet_ldap extends ADORecordSet{
 	var $canSeek = false;
 	var $_entryID; /* keeps track of the entry resource identifier */
 
-	function __construct($queryID,$mode=false)
+	function __construct($queryID, $mode=false)
 	{
-		if ($mode === false) {
-			global $ADODB_FETCH_MODE;
-			$mode = $ADODB_FETCH_MODE;
-		}
-		switch ($mode)
-		{
-		case ADODB_FETCH_NUM:
-			$this->fetchMode = LDAP_NUM;
-			break;
-		case ADODB_FETCH_ASSOC:
-			$this->fetchMode = LDAP_ASSOC;
-			break;
-		case ADODB_FETCH_DEFAULT:
-		case ADODB_FETCH_BOTH:
-		default:
-			$this->fetchMode = LDAP_BOTH;
-			break;
-		}
+		parent::__construct($queryID, $mode);
 
-		parent::__construct($queryID);
+		switch ($mode) {
+			case ADODB_FETCH_NUM:
+				$this->fetchMode = LDAP_NUM;
+				break;
+			case ADODB_FETCH_ASSOC:
+				$this->fetchMode = LDAP_ASSOC;
+				break;
+			case ADODB_FETCH_DEFAULT:
+			case ADODB_FETCH_BOTH:
+			default:
+				$this->fetchMode = LDAP_BOTH;
+				break;
+		}
 	}
 
 	function _initrs()

drivers/adodb-mysqli.inc.php
@@ -1396,10 +1396,7 @@ class ADORecordSet_mysqli extends ADORecordSet{
 
 	function __construct($queryID, $mode = false)
 	{
-		if ($mode === false) {
-			global $ADODB_FETCH_MODE;
-			$mode = $ADODB_FETCH_MODE;
-		}
+		parent::__construct($queryID, $mode);
 
 		switch ($mode) {
 			case ADODB_FETCH_NUM:
@@ -1414,8 +1411,6 @@ class ADORecordSet_mysqli extends ADORecordSet{
 				$this->fetchMode = MYSQLI_BOTH;
 				break;
 		}
-		$this->adodbFetchMode = $mode;
-		parent::__construct($queryID);
 	}
 
 	function _initrs()

drivers/adodb-oci8.inc.php
@@ -1621,12 +1621,10 @@ class ADORecordset_oci8 extends ADORecordSet {
 	/** @var resource Cursor reference */
 	var $_refcursor;
 
-	function __construct($queryID,$mode=false)
+	function __construct($queryID, $mode=false)
 	{
-		if ($mode === false) {
-			global $ADODB_FETCH_MODE;
-			$mode = $ADODB_FETCH_MODE;
-		}
+		parent::__construct($queryID, $mode);
+
 		switch ($mode) {
 			case ADODB_FETCH_ASSOC:
 				$this->fetchMode = OCI_ASSOC;
@@ -1641,8 +1639,6 @@ class ADORecordset_oci8 extends ADORecordSet {
 				break;
 		}
 		$this->fetchMode += OCI_RETURN_NULLS + OCI_RETURN_LOBS;
-		$this->adodbFetchMode = $mode;
-		$this->_queryID = $queryID;
 	}
 
 	/**

drivers/adodb-pdo.inc.php
@@ -853,26 +853,24 @@ class ADORecordSet_pdo extends ADORecordSet {
 	/** @var PDOStatement */
 	var $_queryID;
 
-	function __construct($id,$mode=false)
+	function __construct($queryID, $mode=false)
 	{
-		if ($mode === false) {
-			global $ADODB_FETCH_MODE;
-			$mode = $ADODB_FETCH_MODE;
-		}
-		$this->adodbFetchMode = $mode;
-		switch($mode) {
-		case ADODB_FETCH_NUM: $mode = PDO::FETCH_NUM; break;
-		case ADODB_FETCH_ASSOC:  $mode = PDO::FETCH_ASSOC; break;
+		parent::__construct($queryID, $mode);
 
-		case ADODB_FETCH_BOTH:
-		default: $mode = PDO::FETCH_BOTH; break;
+		switch($mode) {
+			case ADODB_FETCH_NUM:
+				$mode = PDO::FETCH_NUM;
+				break;
+			case ADODB_FETCH_ASSOC:
+				$mode = PDO::FETCH_ASSOC;
+				break;
+			case ADODB_FETCH_BOTH:
+			default:
+				$mode = PDO::FETCH_BOTH;
+				break;
 		}
 		$this->fetchMode = $mode;
-
-		$this->_queryID = $id;
-		parent::__construct($id);
 	}
-
 
 	function Init()
 	{

drivers/adodb-postgres64.inc.php
@@ -950,10 +950,8 @@ class ADORecordSet_postgres64 extends ADORecordSet{
 
 	function __construct($queryID, $mode=false)
 	{
-		if ($mode === false) {
-			global $ADODB_FETCH_MODE;
-			$mode = $ADODB_FETCH_MODE;
-		}
+		parent::__construct($queryID, $mode);
+
 		switch ($mode) {
 			case ADODB_FETCH_NUM:
 				$this->fetchMode = PGSQL_NUM;
@@ -961,17 +959,12 @@ class ADORecordSet_postgres64 extends ADORecordSet{
 			case ADODB_FETCH_ASSOC:
 				$this->fetchMode = PGSQL_ASSOC;
 				break;
-
 			case ADODB_FETCH_DEFAULT:
 			case ADODB_FETCH_BOTH:
 			default:
 				$this->fetchMode = PGSQL_BOTH;
 				break;
 		}
-		$this->adodbFetchMode = $mode;
-
-		// Parent's constructor
-		parent::__construct($queryID);
 	}
 
 	function GetRowAssoc($upper = ADODB_ASSOC_CASE)

drivers/adodb-sqlite.inc.php
@@ -425,14 +425,11 @@ class ADORecordset_sqlite extends ADORecordSet {
 	var $databaseType = "sqlite";
 	var $bind = false;
 
-	function __construct($queryID,$mode=false)
+	function __construct($queryID, $mode=false)
 	{
+		parent::__construct($queryID, $mode);
 
-		if ($mode === false) {
-			global $ADODB_FETCH_MODE;
-			$mode = $ADODB_FETCH_MODE;
-		}
-		switch($mode) {
+		switch ($mode) {
 			case ADODB_FETCH_NUM:
 				$this->fetchMode = SQLITE_NUM;
 				break;
@@ -443,9 +440,6 @@ class ADORecordset_sqlite extends ADORecordSet {
 				$this->fetchMode = SQLITE_BOTH;
 				break;
 		}
-		$this->adodbFetchMode = $mode;
-
-		$this->_queryID = $queryID;
 
 		$this->_inited = true;
 		$this->fields = array();
@@ -461,7 +455,6 @@ class ADORecordset_sqlite extends ADORecordSet {
 
 		return $this->_queryID;
 	}
-
 
 	function FetchField($fieldOffset = -1)
 	{

drivers/adodb-sqlite3.inc.php
@@ -719,12 +719,9 @@ class ADORecordset_sqlite3 extends ADORecordSet {
 	var $_queryID;
 
 	/** @noinspection PhpMissingParentConstructorInspection */
-	function __construct($queryID,$mode=false)
+	function __construct($queryID, $mode=false)
 	{
-		if ($mode === false) {
-			global $ADODB_FETCH_MODE;
-			$mode = $ADODB_FETCH_MODE;
-		}
+		parent::__construct($queryID, $mode);
 		switch($mode) {
 			case ADODB_FETCH_NUM:
 				$this->fetchMode = SQLITE3_NUM;
@@ -736,9 +733,6 @@ class ADORecordset_sqlite3 extends ADORecordSet {
 				$this->fetchMode = SQLITE3_BOTH;
 				break;
 		}
-		$this->adodbFetchMode = $mode;
-
-		$this->_queryID = $queryID;
 
 		$this->_inited = true;
 		$this->fields = array();
@@ -754,7 +748,6 @@ class ADORecordset_sqlite3 extends ADORecordSet {
 
 		return $this->_queryID;
 	}
-
 
 	function FetchField($fieldOffset = -1)
 	{

commit 40e53396b9a8f01ddc36de30321015bed3b9cca7

Incorrect ADORecordset::$fetchMode initialization
Regression introduced by 5c9e24cf98409004da2a5e5bd80eeec82c5a3997

For some drivers using native fetch mode constants, the $fetchMode
property was not set properly as the switch statement assigning its
value was driven by the constructor's $mode parameter instead of the
parent constructor's $adodbFetchMode property.

Fixes #980

drivers/adodb-fbsql.inc.php
@@ -173,7 +173,7 @@ class ADORecordSet_fbsql extends ADORecordSet{
 	{
 		parent::__construct($queryID, $mode);
 
-		switch ($mode) {
+		switch ($this->adodbFetchMode) {
 			case ADODB_FETCH_NUM:
 				$this->fetchMode = FBSQL_NUM;
 				break;

drivers/adodb-ldap.inc.php
@@ -299,7 +299,7 @@ class ADORecordSet_ldap extends ADORecordSet{
 	{
 		parent::__construct($queryID, $mode);
 
-		switch ($mode) {
+		switch ($this->adodbFetchMode) {
 			case ADODB_FETCH_NUM:
 				$this->fetchMode = LDAP_NUM;
 				break;

drivers/adodb-mysqli.inc.php
@@ -1401,7 +1401,7 @@ class ADORecordSet_mysqli extends ADORecordSet{
 	{
 		parent::__construct($queryID, $mode);
 
-		switch ($mode) {
+		switch ($this->adodbFetchMode) {
 			case ADODB_FETCH_NUM:
 				$this->fetchMode = MYSQLI_NUM;
 				break;

drivers/adodb-oci8.inc.php
@@ -1625,7 +1625,7 @@ class ADORecordset_oci8 extends ADORecordSet {
 	{
 		parent::__construct($queryID, $mode);
 
-		switch ($mode) {
+		switch ($this->adodbFetchMode) {
 			case ADODB_FETCH_ASSOC:
 				$this->fetchMode = OCI_ASSOC;
 				break;

drivers/adodb-pdo.inc.php
@@ -857,7 +857,7 @@ class ADORecordSet_pdo extends ADORecordSet {
 	{
 		parent::__construct($queryID, $mode);
 
-		switch($mode) {
+		switch($this->adodbFetchMode) {
 			case ADODB_FETCH_NUM:
 				$mode = PDO::FETCH_NUM;
 				break;

drivers/adodb-postgres64.inc.php
@@ -952,7 +952,7 @@ class ADORecordSet_postgres64 extends ADORecordSet{
 	{
 		parent::__construct($queryID, $mode);
 
-		switch ($mode) {
+		switch ($this->adodbFetchMode) {
 			case ADODB_FETCH_NUM:
 				$this->fetchMode = PGSQL_NUM;
 				break;

drivers/adodb-sqlite.inc.php
@@ -429,7 +429,7 @@ class ADORecordset_sqlite extends ADORecordSet {
 	{
 		parent::__construct($queryID, $mode);
 
-		switch ($mode) {
+		switch ($this->adodbFetchMode) {
 			case ADODB_FETCH_NUM:
 				$this->fetchMode = SQLITE_NUM;
 				break;

drivers/adodb-sqlite3.inc.php
@@ -722,7 +722,7 @@ class ADORecordset_sqlite3 extends ADORecordSet {
 	function __construct($queryID, $mode=false)
 	{
 		parent::__construct($queryID, $mode);
-		switch($mode) {
+		switch($this->adodbFetchMode) {
 			case ADODB_FETCH_NUM:
 				$this->fetchMode = SQLITE3_NUM;
 				break;

drivers/adodb-sybase.inc.php
@@ -319,11 +319,7 @@ class ADORecordset_sybase extends ADORecordSet {
 	{
 		parent::__construct($queryID, $mode);
 
-		if (!$mode) {
-			$this->fetchMode = ADODB_FETCH_ASSOC;
-		} else {
-			$this->fetchMode = $mode;
-		}
+		$this->fetchMode = $this->adodbFetchMode ?: ADODB_FETCH_ASSOC;
 	}
 
 	/*	Returns: an object containing field information.