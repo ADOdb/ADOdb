8bc38a195da39aef00f3740e91b17969c4ac66a8
Damien Regad <dregad@mantisbt.org>
2023-06-11 13:12:44 +0200

    Add magic methods to ADOFetchObj
    This allows hanlding of dynamic properties for PHP 8.2 compatibility,
    without relying on the AllowDynamicProperties attribute.
    
    The constructor accepts an array of (fieldname => value) pairs for easy
    initialization.
    
    Fixes #982

--- adodb.inc.php
+++ adodb.inc.php
@@ -3799,17 +3799,55 @@ http://www.stanford.edu/dept/itss/docs/oracle/10g/server.101/b10759/statements_1
 
 } // end class ADOConnection
 
-
-
-	//==============================================================================================
-	// CLASS ADOFetchObj
-	//==============================================================================================
-
 	/**
-	 * Internal placeholder for record objects. Used by ADORecordSet->FetchObj().
+	 * RecordSet fields data as object.
+	 *
+	 * @see ADORecordSet::fetchObj(), ADORecordSet::fetchObject(),
+	 * @see ADORecordSet::fetchNextObj(), ADORecordSet::fetchNextObject()
 	 */
 	class ADOFetchObj {
-	};
+		/** @var array The RecordSet's fields */
+		protected $data;
+
+		/**
+		 * Constructor.
+		 *
+		 * @param array $fields Associative array with RecordSet's fields (name => value)
+		 */
+		public function __construct(array $fields = [])
+		{
+			$this->data = $fields;
+		}
+
+		public function __set(string $name, $value)
+		{
+			$this->data[$name] = $value;
+		}
+
+		public function __get(string $name)
+		{
+			if (isset($this->data[$name])) {
+				return $this->data[$name];
+			}
+			ADOConnection::outp("Unknown field: $name");
+			return null;
+		}
+
+		public function __isset($name)
+		{
+			return isset($this->data[$name]);
+		}
+
+		public function __debugInfo()
+		{
+			return $this->data;
+		}
+
+		public static function __set_state(array $data)
+		{
+			return new self($data['data']);
+		}
+	}
 
 	/**
 	 * Class ADODB_Iterator_empty
