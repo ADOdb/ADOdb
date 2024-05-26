ADOdb/ADOdb

commit 249b7fb08569ce576cd1bf26098e00701145c6ec

Catch getAssoc() execution error in metaColumns()
When getAssoc() returns false, metaColumns() throws PHP Fatal error on
array_change_key_case, bool given instead of array.

We now check that getAssoc() completed successfully before continuing,
and abort if not.

Fixes #1016

Signed-off-by: Damien Regad <dregad@mantisbt.org>

drivers/adodb-mysqli.inc.php
@@ -944,6 +944,10 @@ class ADODB_mysqli extends ADOConnection {
 				   AND table_name='$table'";
 
 		$schemaArray = $this->getAssoc($SQL);
+		if (!$schemaArray) {
+			return false;
+		}
+
 		$schemaArray = array_change_key_case($schemaArray,CASE_LOWER);
 
 		$rs = $this->Execute(sprintf($this->metaColumnsSQL,$table));

commit 5f589f64cea42c792929832cffe26825831f8c94

Reset fetch mode before return from MetaColumns()
Commit 249b7fb08569ce576cd1bf26098e00701145c6ec (PR #1017) introduced a
regression caused by returning from the function without resetting
$ADODB_FETCH_MODE to its original value.

Fixes #1016

drivers/adodb-mysqli.inc.php
@@ -963,13 +963,11 @@ class ADODB_mysqli extends ADOConnection {
 				   AND table_name='$table'";
 
 		$schemaArray = $this->getAssoc($SQL);
-		if (!$schemaArray) {
-			return false;
+		if (is_array($schemaArray)) {
+			$schemaArray = array_change_key_case($schemaArray,CASE_LOWER);
+			$rs = $this->Execute(sprintf($this->metaColumnsSQL,$table));
 		}
 
-		$schemaArray = array_change_key_case($schemaArray,CASE_LOWER);
-
-		$rs = $this->Execute(sprintf($this->metaColumnsSQL,$table));
 		if (isset($savem)) $this->SetFetchMode($savem);
 		$ADODB_FETCH_MODE = $save;
 		if (!is_object($rs))