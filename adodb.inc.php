bc0d3e05ce89f3c73c24d1cb78de57793d1afe3a
Damien Regad <dregad@mantisbt.org>
2025-01-25 02:10:09 +0100

    Bump version to 5.22.8

--- adodb.inc.php
+++ adodb.inc.php
@@ -198,7 +198,7 @@ if (!defined('_ADODB_LAYER')) {
 		/**
 		 * ADODB version as a string.
 		 */
-		$ADODB_vers = 'v5.22.8-dev  Unreleased';
+		$ADODB_vers = 'v5.22.8  2025-01-25';
 
 		/**
 		 * Determines whether recordset->RecordCount() is used.
