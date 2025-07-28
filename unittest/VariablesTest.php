0c35306a71b5979813e62b6007a82d85e64d2ec5
Mark Newnham <mark@newnhams.com>
2025-07-28 17:28:12 -0600

    PSR 12

--- unittest/VariablesTest.php
+++ unittest/VariablesTest.php
@@ -27,28 +27,40 @@ use PHPUnit\Framework\TestCase;
  */
 class VariablesTest extends TestCase
 {
-	protected ?object $db;
-	protected ?string $adoDriver;
+    protected ?object $db;
+    protected ?string $adoDriver;
     protected ?object $dataDictionary;
 
     protected bool $skipFollowingTests = false;
 
-	public function setUp(): void
-	{
+    /**
+     * Set up the test environment
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
 
         $this->db        = $GLOBALS['ADOdbConnection'];
-		$this->adoDriver = $GLOBALS['ADOdriver'];
+        $this->adoDriver = $GLOBALS['ADOdriver'];
         
-	}
-	
-	public function tearDown(): void
-	{
-		//$this->db->execute("DROP TABLE IF EXISTS {$this->testTableName}");
-	}
+    }
+    
+    /**
+     * Tear down the test environment
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        //$this->db->execute("DROP TABLE IF EXISTS {$this->testTableName}");
+    }
 
     /**
      * Test for {@see $ADODB_QUOTE_FIELDNAMES}
+     * 
      * @link https://adodb.org/dokuwiki/doku.php?id=v5:reference:adodb_quote_fieldnames
+     * 
      * @return void
      */
     public function testQuotingExecute(): void
@@ -73,29 +85,42 @@ class VariablesTest extends TestCase
         
         $success = $this->db->execute($sql);
 
-        $this->assertIsObject($success, 'Data insertion should not succed using Unquoted field and table names');
+        $this->assertIsObject(
+            $success, 
+            'Data insertion should not succed using Unquoted field and table names'
+        );
 
         $count = $this->db->getOne("SELECT COUNT(*) FROM `table_name`");
 
-        $this->assertEquals(0, $count, 'Data insertion should not have succeeded using Unquoted field and table names');
+        $this->assertEquals(
+            0, 
+            $count, 
+            'Data insertion should not have succeeded using Unquoted field and table names'
+        );
+        
         /*
         * Now activate the quoting of field and table names
         */
         $ADODB_QUOTE_FIELDNAMES = true;
 
-         $sql = $this->db->getInsertSQL(
+        $sql = $this->db->getInsertSQL(
             $template,
             $ar
         );
         
         $success = $this->db->execute($sql);
 
-        $this->assertIsObject($success, 'Data insertion failed Using Quoted field and table names. The failing SQL was: ' . $sql);
+        $this->assertIsObject(
+            $success, 
+            'Data insertion failed Using Quoted field and table names. The failing SQL was: ' . $sql
+        );
     }
 
     /**
      * Test for {@see $ADODB_FETCH_MODE}
+     * 
      * @link https://adodb.org/dokuwiki/doku.php?id=v5:reference:adodb_fetch_mode
+     * 
      * @return void
      */
     public function testFetchMode(): void
@@ -108,43 +133,61 @@ class VariablesTest extends TestCase
         $sql = "SELECT * FROM testtable_1";
         $testRow = $this->db->getRow($sql);
 
-        $this->assertArrayHasKey('id',$testRow, 'Row should have an id column');        
+        $this->assertArrayHasKey(
+            'id',
+            $testRow,
+            'Row should have an id column'
+        );        
 
         $ADODB_FETCH_MODE = ADODB_FETCH_NUM;
 
         $testRow = $this->db->getRow($sql);
 
-        $this->assertArrayHasKey(0,$testRow, 'Row should have a numeric column');
+        $this->assertArrayHasKey(
+            0,
+            $testRow, 
+            'Row should have a numeric column'
+        );
     
         $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
     }
 
     /**
      * Test for {@see $ADODB_GETONE_EOF}
+     * 
      * @link https://adodb.org/dokuwiki/doku.php?id=v5:reference:adodb_getone_eof
+     * 
      * @return void
      */
     public function testGetOneEof(): void
     {
         global $ADODB_GETONE_EOF;
-
-             
+     
         $sql = 'select varchar_field from testtable_1 where id=9999';
         $test = $this->db->getOne($sql);
 
-        $this->assertEquals($test, false, 'getOne by default should return false when no row is found');
+        $this->assertEquals(
+            $test, 
+            false, 
+            'getOne by default should return false when no row is found'
+        );
 
         $ADODB_GETONE_EOF = -1;
 
         $test = $this->db->getOne($sql);
 
-        
-        $this->assertEquals($test, -1, 'getOne should now flag by -1 when no row is found');
+        $this->assertEquals(
+            $test,
+            -1,
+            'getOne should now flag by -1 when no row is found'
+        );
 
     }
     /**
      * Test for {@see $ADODB_COUNTRECS}
+     * 
      * @link https://adodb.org/dokuwiki/doku.php?id=v5:reference:adodb_countrecs
+     * 
      * @return void
      */ 
     public function testCountRecs(): void
@@ -155,13 +198,21 @@ class VariablesTest extends TestCase
         $sql = "SELECT * FROM testtable_1";
         $result = $this->db->Execute($sql);
 
-        $this->assertEquals(11,$result->recordCount(), 'With ADODB_COUNTRECS set to true, the record count should be 11');        
+        $this->assertEquals(
+            11,
+            $result->recordCount(), 
+            'With ADODB_COUNTRECS set to true, the record count should be 11'
+        );        
 
         $ADODB_COUNTRECS = false;
 
         $result = $this->db->Execute($sql);
         
-        $this->assertEquals(-1,$result->recordCount(), 'With ADODB_COUNTRECS set to false, the record count should be -1');
+        $this->assertEquals(
+            -1,
+            $result->recordCount(), 
+            'With ADODB_COUNTRECS set to false, the record count should be -1'
+        );
 
         $ADODB_COUNTRECS = true; // Reset to default for other tests
     }
