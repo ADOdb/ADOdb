91870cfdbb91f02594c876496acfd46b18e57d14
Mark Newnham <mark@newnhams.com>
2025-08-02 20:15:17 -0600

    Add empty driver-specific tesmplates

---
+++ unittest/MssqlnativeDriverTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Tests cases for the mssqlnative driver of ADOdb.
+ * Try to write database-agnostic tests where possible.
+ *
+ * This file is part of ADOdb, a Database Abstraction Layer library for PHP.
+ *
+ * @package ADOdb
+ * @link https://adodb.org Project's web site and documentation
+ * @link https://github.com/ADOdb/ADOdb Source code and issue tracker
+ *
+ * The ADOdb Library is dual-licensed, released under both the BSD 3-Clause
+ * and the GNU Lesser General Public Licence (LGPL) v2.1 or, at your option,
+ * any later version. This means you can use it in proprietary products.
+ * See the LICENSE.md file distributed with this source code for details.
+ * @license BSD-3-Clause
+ * @license LGPL-2.1-or-later
+ *
+ * @copyright 2025 Damien Regad, Mark Newnham and the ADOdb community
+ */
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class MetaFunctionsTest
+ *
+ * Test cases for for ADOdb MetaFunctions
+ */
+class FirebirdDriverTest extends TestCase
+{
+    protected ?object $db;
+    protected ?string $adoDriver;
+    protected ?object $dataDictionary;
+
+    protected bool $skipFollowingTests = false;
+
+    /**
+     * Set up the test environment
+     *
+     * @return void
+     */
+    public function setup(): void
+    {
+
+        $this->db        = &$GLOBALS['ADOdbConnection'];
+        $this->adoDriver = $GLOBALS['ADOdriver'];
+
+        if ($this->adoDriver !== 'mssqlnative') {
+            $this->skipFollowingTests = true;
+            $this->markTestSkipped(
+                'This test is only applicable for the mssqlnative driver'
+            );
+        }
+        
+    }
+    
+    /**
+     * Tear down the test environment
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        
+    }
+}
