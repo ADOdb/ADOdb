c5b49bf4c7c8e37c225ea8665b2fdee5f56c11a0
Mark Newnham <mark@newnhams.com>
2025-08-06 18:58:01 -0600

    Improve test schema for tables

---
+++ unittest/DatabaseSetup/db2/table-schema.sql
@@ -0,0 +1,70 @@
+-- Standard format for the unit testing
+-- All drivers must have the same table and column structure
+-- so that all tests run the same way
+-- but this is native inserts so the column types may differ across databases
+
+-- insertion_table will be built by createTable tests
+DROP TABLE IF EXISTS insertion_table;
+DROP TABLE IF EXISTS insertion_table_renamed;
+
+DROP TABLE IF EXISTS testtable_3;
+-- Must drop testtable_2 before testtable_1 because of foreign key constraints
+DROP TABLE IF EXISTS testtable_2;
+DROP TABLE IF EXISTS testtable_1;
+-- This table will be built by XMLschema tests
+DROP TABLE IF EXISTS testxmltable_1;
+
+-- Testtable_1 is used to test the basic functionality of the meta functions
+-- It has a variety of data types but contains no data
+CREATE TABLE testtable_1 (
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+	varchar_field VARCHAR(20),
+	datetime_field DATETIME,
+	date_field DATE,
+	integer_field SMALLINT NOT NULL DEFAULT 0,
+	decimal_field DECIMAL(12,2) DEFAULT 0,
+	boolean_field BIT DEFAULT 0,
+    empty_field VARCHAR(240) DEFAULT '',
+	number_run_field SMALLINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+CREATE	UNIQUE INDEX vdx1 ON testtable_1 (varchar_field);
+CREATE	UNIQUE INDEX vdx2 ON testtable_1 (integer_field,date_field);
+CREATE	UNIQUE INDEX vdx3 ON testtable_1 (number_run_field);
+
+-- testtable_2 is used to test foreign keys
+-- There is no data in this table
+-- Currently missing support for composite foreign key tests in DB2 driver
+-- This is solely because I don't know how to create a composite foreign key in DB2
+CREATE TABLE testtable_2 (
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+	integer_field SMALLINT NOT NULL DEFAULT 0,
+	date_field DATE,
+    PRIMARY KEY (id)
+);
+
+-- Testtable_1 is used to test the basic functionality of the meta functions
+-- It has a variety of data types but contains no data
+CREATE TABLE testtable_1 (
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+	varchar_field VARCHAR(20),
+	datetime_field DATETIME,
+	date_field DATE,
+	integer_field SMALLINT NOT NULL DEFAULT 0,
+	decimal_field DECIMAL(12,2) DEFAULT 0,
+	boolean_field BIT DEFAULT 0,
+    empty_field VARCHAR(240) DEFAULT '',
+	number_run_field SMALLINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+CREATE	UNIQUE INDEX vdx31 ON testtable_3 (varchar_field);
+CREATE	UNIQUE INDEX vdx33 ON testtable_3 (number_run_field);
+
+-- This table is used to test the quoting of table and field names
+-- It uses a reserved word as the table name and column names
+DROP TABLE IF EXISTS "table_name";
+CREATE TABLE "table_name" (
+	"id" INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+	"column_name" VARCHAR(20),
+	PRIMARY KEY("id")
+);
---
+++ unittest/DatabaseSetup/table3-data.sql
@@ -0,0 +1,14 @@
+-- Populate testtable_3 with sample data
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 1','2025-01-01 00:00:01','2025-01-01',9001,1000.01,1);
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 2','2025-01-01 01:00:01','2025-02-01',9002,1000.11,2);
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 3','2025-01-01 02:00:01','2025-03-01',9003,1000.21,3);
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 4','2025-01-01 03:00:01','2025-04-01',9004,1000.31,4);
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 5','2025-01-01 04:00:01','2025-05-01',9005,1000.41,5);
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 6','2025-01-01 05:00:01','2025-06-01',9006,1000.51,6);
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 7','2025-01-01 06:00:01','2025-07-01',9007,1000.61,7);
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 8','2025-01-01 07:00:01','2025-08-01',9008,1000.71,8);
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 9','2025-01-01 08:00:01','1959-08-29',9009,1000.81,9);
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 10','2025-01-01 09:00:01','2025-10-01',9010,1000.91,10);
+INSERT INTO testtable_3 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 11','2025-01-01 23:00:01','1725-11-01',-9011,-1000.11,11);
+-- Force a foreign key constraint into testtable_1 for later foreign key tests
+INSERT INTO testtable_1 (varchar_field,	datetime_field,date_field, integer_field,decimal_field,number_run_field) values ('LINE 2','2025-01-01 01:00:01','2025-02-01',9002,1000.11,2);
