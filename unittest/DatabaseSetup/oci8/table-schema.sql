cb7e58acd4d4a6a2be4bbb86b02d01359876b532
Mark Newnham <mark@newnhams.com>
2025-08-09 20:09:17 -0600

    Add trigger plus sequewnce for auto-increment column

--- unittest/DatabaseSetup/oci8/table-schema.sql
+++ unittest/DatabaseSetup/oci8/table-schema.sql
@@ -1,29 +1,20 @@
--- Standard format for the unit testing
--- All drivers must have the same table and column structure
--- so that all tests run the same way
--- but this is native inserts so the column types may differ across databases
-
--- insertion_table will be built by createTable tests
-
 DROP TABLE IF EXISTS insertion_table;
 DROP TABLE IF EXISTS insertion_table_renamed;
 
 DROP TABLE IF EXISTS testtable_3;
 
--- Must drop testtable_2 before testtable_1 because of foreign key constraints
-
 DROP TABLE IF EXISTS testtable_2;
 DROP TABLE IF EXISTS testtable_1;
 
--- This table will be built by XMLschema tests
-
 DROP TABLE IF EXISTS testxmltable_1;
 
+DROP SEQUENCE testtable_3_seq;
+DROP SEQUENCE table_name_seq;
 
 CREATE TABLE testtable_1 (
 	id INTEGER NOT NULL,
     varchar_field VARCHAR(20),
-    datetime_field TIMESTAMP,
+    datetime_field VARCHAR(20),
     date_field DATE,
     integer_field SMALLINT NOT NULL,
     decimal_field NUMBER(12,2),
@@ -36,19 +27,36 @@ CREATE	UNIQUE INDEX vdx2 ON testtable_1 (integer_field,date_field);
 CREATE	UNIQUE INDEX vdx3 ON testtable_1 (number_run_field);
 
 
+CREATE SEQUENCE testtable_1_seq
+    INCREMENT BY 1
+    START WITH 1;
+
+CREATE OR REPLACE TRIGGER testable_1_t BEFORE insert ON testtable_1 FOR EACH ROW WHEN (NEW.id IS NULL OR NEW.id=0) BEGIN select testtable_1_seq.nextval into :new.id from dual; END; ;
+
 
 CREATE TABLE testtable_2 (
 	id INTEGER NOT NULL,
 	integer_field SMALLINT NOT NULL,
 	date_field DATE,
 	blob_field BLOB
-    
 );
+CREATE UNIQUE INDEX vdx21 ON testtable_2 (integer_field,date_field);
+ 
+ALTER TABLE testtable_2 ADD CONSTRAINT fk_column
+    FOREIGN KEY (integer_field,date_field)
+    REFERENCES testtable_1 (integer_field,date_field);
+
+CREATE SEQUENCE testtable_2_seq
+    INCREMENT BY 1
+    START WITH 1;
+
+CREATE OR REPLACE TRIGGER testable_2_t BEFORE insert ON testtable_2 FOR EACH ROW WHEN (NEW.id IS NULL OR NEW.id=0) BEGIN select testtable_2_seq.nextval into :new.id from dual; END; ;
+
 
 CREATE TABLE testtable_3 (
 id INTEGER NOT NULL,
     varchar_field VARCHAR(20),
-    datetime_field TIMESTAMP,
+    datetime_field VARCHAR(20),
     date_field DATE,
     integer_field SMALLINT NOT NULL,
     decimal_field NUMBER(12,2),
@@ -56,8 +64,15 @@ id INTEGER NOT NULL,
     empty_field VARCHAR(240) DEFAULT '',
     number_run_field SMALLINT NOT NULL
 );
+
 CREATE	UNIQUE INDEX vdx31 ON testtable_3 (varchar_field);
 CREATE	UNIQUE INDEX vdx33 ON testtable_3 (number_run_field);
+
+CREATE SEQUENCE testtable_3_seq
+    INCREMENT BY 1
+    START WITH 1;
+
+CREATE OR REPLACE TRIGGER testable_3_t BEFORE insert ON testtable_3 FOR EACH ROW WHEN (NEW.id IS NULL OR NEW.id=0) BEGIN select testtable_3_seq.nextval into :new.id from dual; END; ;
 
 DROP TABLE IF EXISTS "table_name";
 CREATE TABLE "table_name" (
