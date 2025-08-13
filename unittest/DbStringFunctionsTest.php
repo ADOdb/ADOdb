75151ff52bdc6edf84dfb75f89611293238d9958
Mark Newnham <mark@newnhams.com>
2025-08-12 20:16:11 -0600

    Add additional tests based on fetch mode

--- unittest/DbStringFunctionsTest.php
+++ unittest/DbStringFunctionsTest.php
@@ -62,14 +62,17 @@ class DbStringFunctionsTest extends TestCase
         * Blank out the empty_field column first to ensure that
         * the total number of rows updated is correct
         */
-        $SQL = "UPDATE testtable_1 SET empty_field = null";
+        $SQL = "UPDATE testtable_3 SET empty_field = null";
         
+        $this->db->startTrans();
         $this->db->Execute($SQL);
+        $this->db->CompleteTrans();
+        $SQL = "UPDATE testtable_3 SET empty_field = {$this->db->qstr($testString)}";
+        
+        $this->db->startTrans();
+        $this->db->Execute($SQL);
+        $this->db->CompleteTrans();
 
-        $SQL = "UPDATE testtable_1 SET empty_field = {$this->db->qstr($testString)}";
-        
-        $this->db->Execute($SQL);
-        
         $expectedValue = 11;
         $actualValue = $this->db->Affected_Rows();
 
@@ -81,10 +84,12 @@ class DbStringFunctionsTest extends TestCase
         );
 
         // Now we will check the value in the empty_field column
-        $sql = "SELECT empty_field FROM testtable_1";
+        $sql = "SELECT empty_field FROM testtable_3";
 
+        $this->db->startTrans();
         $returnValue = $this->db->getOne($sql);
-
+        $this->db->CompleteTrans();
+        
         $testResult = preg_match('/^(Famed author James O)[\\\'](\'Sullivan)$/', $returnValue);
                 
         $this->assertSame(
@@ -115,7 +120,7 @@ class DbStringFunctionsTest extends TestCase
             'p1' => $this->db->addQ($testString)
         );
 
-        $SQL = "UPDATE testtable_1 SET empty_field = $p1";
+        $SQL = "UPDATE testtable_3 SET empty_field = $p1";
         
         $this->db->Execute($SQL, $bind);
 
@@ -132,7 +137,7 @@ class DbStringFunctionsTest extends TestCase
         $returnValue = $this->db->getOne($sql);
 
         // Now we will check the value in the empty_field column
-        $sql = "SELECT empty_field FROM testtable_1";
+        $sql = "SELECT empty_field FROM testtable_3";
 
         $returnValue = $this->db->getOne($sql);
 
@@ -151,14 +156,18 @@ class DbStringFunctionsTest extends TestCase
      * 
      * @link https://adodb.org/dokuwiki/doku.php?id=v5:reference:connection:concat
      * 
+     * @dataProvider providerTestConcat
+     * 
      * @return void
      */
-    public function testConcat(): void
+    public function testConcat(int $fetchMode, string $firstColumn, string $secondColumn): void
     {
         
         /*
         * Find a record that has a varchar_field value
         */
+
+        $this->db->setFetchMode($fetchMode);
 
         $sql = "SELECT number_run_field, varchar_field 
                   FROM testtable_1 
@@ -169,15 +178,15 @@ class DbStringFunctionsTest extends TestCase
 
         $expectedValue = sprintf(
             '%s|%s',
-            $row['varchar_field'],
-            $row['varchar_field']
+            $row[$secondColumn],
+            $row[$secondColumn]
         );
 
         $field = $this->db->Concat('varchar_field', "'|'", 'varchar_field');
         
         $sql = "SELECT $field 
                   FROM testtable_1 
-                 WHERE number_run_field={$row['number_run_field']}";
+                 WHERE number_run_field={$row[$firstColumn]}";
 
 
         $result = $this->db->getOne($sql);
@@ -187,22 +196,78 @@ class DbStringFunctionsTest extends TestCase
             $result,
             sprintf('3 value concat should return %s', $expectedValue)
         );
+
+        $this->db->setFetchMode(ADODB_FETCH_ASSOC);
             
     }
 
+    /**
+     * Data provider for {@see testConcat()}
+     *
+     * @return array [int $fetchmode, string $number_run_column, string $varchar_column]
+     */
+    public function providerTestConcat(): array
+    {
+        
+        switch (ADODB_ASSOC_CASE) {
+            case ADODB_ASSOC_CASE_UPPER:
+            
+            return [
+                'FETCH_ASSOC,ASSOC_CASE_UPPER' => 
+                array(
+                    ADODB_FETCH_ASSOC, 
+                    'NUMBER_RUN_FIELD',
+                    'VARCHAR_FIELD',
+                ),
+                'FETCH_NUM,ASSOC_CASE_UPPER' =>
+                array(
+                    0=>ADODB_FETCH_NUM, 
+                    1=>"0",
+                    2=>"1"
+                
+                )
+            ];
+            break;
+            
+            case ADODB_ASSOC_CASE_LOWER:
+            default:
+            
+            return [
+                'FETCH_ASSOC,ASSOC_CASE_LOWER' => [
+                    ADODB_FETCH_ASSOC, 
+                    'number_run_field',
+                    'varchar_field',
+                ],
+                'FETCH_NUM,ASSOC_CASE_UPPER' => [
+                    0=>ADODB_FETCH_NUM, 
+                    1=>"0",
+                    2=>"1"
+                ]
+            ];
+ 
+            break;
+
+        }
+       
+    }
 
     /**
      * Test for {@see ADODConnection::ifNull()}
      * 
      * @link https://adodb.org/dokuwiki/doku.php?id=v5:reference:connection:ifnull
      * 
+     * @dataProvider providerTestIfnull
+     * 
      * @return void
      */
-    public function testIfNull(): void
+    public function testIfNull(int $fetchMode, string $firstColumn, string $secondColumn): void
     {
 
         
-        $sql = "SELECT number_run_field, date_field 
+
+        $this->db->setFetchMode($fetchMode);
+
+        $sql = "SELECT number_run_field, decimal_field 
                   FROM testtable_1 
                  WHERE date_field IS NOT NULL";
 
@@ -212,36 +277,88 @@ class DbStringFunctionsTest extends TestCase
         * Set up a test record that has a NULL value
         */
         $sql = "UPDATE testtable_1 
-                   SET date_field = null 
-                 WHERE number_run_field={$row['number_run_field']}";
+                   SET decimal_field = null 
+                 WHERE number_run_field={$row[$firstColumn]}";
 
+       
+        $this->db->startTrans();
         $this->db->Execute($sql);
-        
+        $this->db->CompleteTrans();
         /*
         * Now get a weird value back from the ifnull function
         */
-        $nineteenSeventy = $this->db->dbDate('1970-01-01');
-        $sql = "SELECT {$this->db->ifNull('date_field',$nineteenSeventy)} 
+       
+        $sql = "SELECT {$this->db->ifNull('decimal_field', 8675304)} 
                   FROM testtable_1 
-                 WHERE number_run_field=2";
+                 WHERE number_run_field={$row[$firstColumn]}";
 
         $expectedResult = $this->db->getOne($sql);
         
         $this->assertSame(
-            '1970-01-01',
+            '8675304',
             $expectedResult,
-            'Test of ifnull function ' . $this->db->ifNull('date_field',$nineteenSeventy)
+            'Test of ifnull function  should return 8675304'
         );
 
         /*
         * Reset the date_field to a non-null value
         */
         $sql = "UPDATE testtable_1 
-                   SET date_field = {$this->db->dbDate($row['date_field'])} 
-                 WHERE number_run_field={$row['number_run_field']}";
+                   SET decimal_field = {$row[$secondColumn]} 
+                 WHERE number_run_field={$row[$firstColumn]}";
 
+        $this->db->startTrans();
         $this->db->Execute($sql);
+        $this->db->CompleteTrans();
+      
+         $this->db->setFetchMode(ADODB_FETCH_ASSOC);
             
+    }
+
+    /**
+     * Data provider for {@see testIfnull()}
+     *
+     * @return array [int fetchmode, string number_run column, string date column]
+     */
+    public function providerTestIfnull(): array
+    {
+        
+        
+        switch (ADODB_ASSOC_CASE) {
+            case ADODB_ASSOC_CASE_UPPER:
+            return [
+                'FETCH_ASSOC,ASSOC_CASE_UPPER' => [
+                    ADODB_FETCH_ASSOC, 
+                    'NUMBER_RUN_FIELD',
+                    'DECIMAL_FIELD',
+                ],
+                'FETCH_NUM,ASSOC_CASE_UPPER' => [
+                    0=>ADODB_FETCH_NUM, 
+                    1=>"0",
+                    2=>"1"
+    
+                ]
+            ];
+            break;
+            case ADODB_ASSOC_CASE_LOWER:
+            default:
+            return [
+                'FETCH_ASSOC,ASSOC_CASE_LOWER' => [
+                    ADODB_FETCH_ASSOC, 
+                    'number_run_field',
+                    'decimal_field',
+                ],
+                'FETCH_NUM,ASSOC_CASE_UPPER' => [
+                    0=>ADODB_FETCH_NUM, 
+                    1=>"0",
+                    2=>"1"
+
+                ]
+            ];
+            break;
+
+        }
+       
     }
 
 }
