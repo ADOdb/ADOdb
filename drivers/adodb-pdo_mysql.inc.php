3f0fa0dfbedb25603eee0c290fcc489fd6e48704
Damien Regad <dregad@mantisbt.org>
2025-09-28 18:21:58 +0200

    PDO mysql: change $sysDate/$sysTimestamp default
    Porting change from 416d279f128ab77d9d1b7b999087696e05113b1e.
    
    (cherry picked from commit f974ea3ec1711eac1f7e35b13237c98683da17b3)

--- drivers/adodb-pdo_mysql.inc.php
+++ drivers/adodb-pdo_mysql.inc.php
@@ -27,8 +27,8 @@ class ADODB_pdo_mysql extends ADODB_pdo {
 		FROM INFORMATION_SCHEMA.TABLES
 		WHERE TABLE_SCHEMA=";
 	var $metaColumnsSQL = "SHOW COLUMNS FROM `%s`";
-	var $sysDate = 'CURDATE()';
-	var $sysTimeStamp = 'NOW()';
+	var $sysDate = '(CURDATE())';
+	var $sysTimeStamp = '(NOW())';
 	var $hasGenID = true;
 	
 	/*
