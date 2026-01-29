146c7149216c76c6e6e2a1f55396fa4dd5bf2313
Mark Newnham <mark@newnhams.com>
2026-01-29 14:50:51 -0700

    fetchField should return false if call to column is outside bounds of query

--- drivers/adodb-mssqlnative.inc.php
+++ drivers/adodb-mssqlnative.inc.php
@@ -1203,6 +1203,9 @@ class ADORecordset_mssqlnative extends ADORecordSet {
 	*/
 	private function _fetchField($fieldOffset = -1)
 	{
+		if ($fieldOffset < -1 || $fieldOffset > $this->_numOfFields) {
+			return false;
+		}
 		if ($this->fieldObjectsRetrieved) {
 			if ($this->fieldObjectsCache) {
 				// Already got the information
