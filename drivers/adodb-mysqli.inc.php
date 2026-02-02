90d48740b7d67698bab16e6786c4e94ccce18e6b
Mark Newnham <mark@newnhams.com>
2026-02-02 15:11:01 -0700

    Consolidate metaType methods to single reference method

--- drivers/adodb-mysqli.inc.php
+++ drivers/adodb-mysqli.inc.php
@@ -1810,6 +1810,16 @@ class ADORecordSet_mysqli extends ADORecordSet{
 	 */
 	function metaType($t, $len = -1, $fieldObj = false)
 	{
+		
+		if (!$this->connection->dataDictionary) {
+			$this->connection->dataDictionary = newDataDictionary($this->connection);
+		}
+
+		/*
+		* Hand off to the data dictionary
+		*/
+		return $this->connection->dataDictionary->metaTables($t, $len, $fieldObj);
+
 		if (is_object($t)) {
 			$fieldObj = $t;
 			$t = $fieldObj->type;
@@ -1930,6 +1940,12 @@ class ADORecordSet_array_mysqli extends ADORecordSet_array
 	 */
 	function MetaType($t, $len = -1, $fieldObj = false)
 	{
+		
+		if (!$this->connection->dataDictionary) {
+			$this->connection->dataDictionary = newDataDictionary($this->connection);
+		}
+		return $this->connection->dataDictionary->metaTables($t, $len, $fieldObj);
+		
 		if (is_object($t)) {
 			$fieldObj = $t;
 			$t = $fieldObj->type;
