ADOdb/ADOdb

commit 5f589f64cea42c792929832cffe26825831f8c94

Reset fetch mode before return from MetaColumns()
Commit 249b7fb08569ce576cd1bf26098e00701145c6ec (PR #1017) introduced a
regression caused by returning from the function without resetting
$ADODB_FETCH_MODE to its original value.

Fixes #1016

drivers/adodb-mysqli.inc.php
@@ -963,13 +963,11 @@ class ADODB_mysqli extends ADOConnection {
 				   AND table_name='$table'";
 
 		$schemaArray = $this->getAssoc($SQL);
-		if (!$schemaArray) {
-			return false;
+		if (is_array($schemaArray)) {
+			$schemaArray = array_change_key_case($schemaArray,CASE_LOWER);
+			$rs = $this->Execute(sprintf($this->metaColumnsSQL,$table));
 		}
 
-		$schemaArray = array_change_key_case($schemaArray,CASE_LOWER);
-
-		$rs = $this->Execute(sprintf($this->metaColumnsSQL,$table));
 		if (isset($savem)) $this->SetFetchMode($savem);
 		$ADODB_FETCH_MODE = $save;
 		if (!is_object($rs))

commit dedd763d9de3f5aed2b192479bb96d7fc9833175

Fix metaColummns() throwing exception
Calling metaColumns() on a non-existing table triggers an unexpected
Exception when using adodb-exceptions.inc.php. The method should just
return false in this case.

The problem was due to an incorrect evaluation of the table existence
check's return value, resulting in unwanted execution of a SHOW COLUMNS
statement which returned MySQL error 1146 (42S02): Table doesn't exist.

Regression introduced by 5f589f64cea42c792929832cffe26825831f8c94 (see
Issue #1016).

Fixes #1212

drivers/adodb-mysqli.inc.php
@@ -1014,9 +1014,11 @@ class ADODB_mysqli extends ADOConnection {
 				   AND table_name='$table'";
 
 		$schemaArray = $this->getAssoc($SQL);
-		if (is_array($schemaArray)) {
+		if ($schemaArray) {
 			$schemaArray = array_change_key_case($schemaArray,CASE_LOWER);
 			$rs = $this->Execute(sprintf($this->metaColumnsSQL,$table));
+		} else {
+			$rs = false;
 		}
 
 		if (isset($savem)) $this->SetFetchMode($savem);